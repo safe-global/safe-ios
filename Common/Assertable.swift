bdbc6f4e55e2c4b9cbcf966d38a0ec3e58ec1c78
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-04-16 13:23:52 +0200

    WA-271 Added new session implementation

--- Common/Assertable.swift
+++ Common/Assertable.swift
@@ -7,13 +7,27 @@ import Foundation
 public protocol Assertable {
 
     func assertArgument(_ assertion: @autoclosure () -> Bool, _ error: Swift.Error) throws
-
+    func assertNil(_ assertion: @autoclosure () -> Any?, _ error: Swift.Error) throws
+    func assertTrue(_ assertion: @autoclosure () -> Bool, _ error: Swift.Error) throws
+    func assertFalse(_ assertion: @autoclosure () -> Bool, _ error: Swift.Error) throws
 }
 
 public extension Assertable {
 
     func assertArgument(_ assertion: @autoclosure () -> Bool, _ error: Swift.Error) throws {
         if !assertion() { throw error }
+    }
+
+    func assertNil(_ assertion: @autoclosure () -> Any?, _ error: Swift.Error) throws {
+        if assertion() != nil { throw error }
+    }
+
+    func assertTrue(_ assertion: @autoclosure () -> Bool, _ error: Swift.Error) throws {
+        if !assertion() { throw error }
+    }
+
+    func assertFalse(_ assertion: @autoclosure () -> Bool, _ error: Swift.Error) throws {
+        try assertTrue(!assertion(), error)
     }
 
 }
