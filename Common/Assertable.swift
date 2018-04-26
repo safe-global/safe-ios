0151e4468d911baadb262193e639f912669d3449
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-04-26 15:33:12 +0200

    WA-271 Implemented Gatekeeper repository
    - Added throws to auto closures in Assertable
    - Added unit and integration tests
    - Added init in the app delegate

--- Common/Assertable.swift
+++ Common/Assertable.swift
@@ -6,51 +6,51 @@ import Foundation
 
 public protocol Assertable {
 
-    func assertArgument(_ assertion: @autoclosure () -> Bool, _ error: Swift.Error) throws
-    func assertNil(_ assertion: @autoclosure () -> Any?, _ error: Swift.Error) throws
-    func assertNotNil(_ assertion: @autoclosure () -> Any?, _ error: Swift.Error) throws
-    func assertTrue(_ assertion: @autoclosure () -> Bool, _ error: Swift.Error) throws
-    func assertFalse(_ assertion: @autoclosure () -> Bool, _ error: Swift.Error) throws
-    func assertEqual<T>(_ expression1: @autoclosure () -> T,
-                        _ expression2: @autoclosure () -> T,
+    func assertArgument(_ assertion: @autoclosure () throws -> Bool, _ error: Swift.Error) throws
+    func assertNil(_ assertion: @autoclosure () throws -> Any?, _ error: Swift.Error) throws
+    func assertNotNil(_ assertion: @autoclosure () throws -> Any?, _ error: Swift.Error) throws
+    func assertTrue(_ assertion: @autoclosure () throws -> Bool, _ error: Swift.Error) throws
+    func assertFalse(_ assertion: @autoclosure () throws -> Bool, _ error: Swift.Error) throws
+    func assertEqual<T>(_ expression1: @autoclosure () throws -> T,
+                        _ expression2: @autoclosure () throws -> T,
                         _ error: Swift.Error) throws where T: Equatable
-    func assertNotEqual<T>(_ expression1: @autoclosure () -> T,
-                           _ expression2: @autoclosure () -> T,
+    func assertNotEqual<T>(_ expression1: @autoclosure () throws -> T,
+                           _ expression2: @autoclosure () throws -> T,
                            _ error: Swift.Error) throws where T: Equatable
 }
 
 public extension Assertable {
 
-    func assertArgument(_ assertion: @autoclosure () -> Bool, _ error: Swift.Error) throws {
-        if !assertion() { throw error }
+    func assertArgument(_ assertion: @autoclosure () throws -> Bool, _ error: Swift.Error) throws {
+        if try !assertion() { throw error }
     }
 
-    func assertNil(_ assertion: @autoclosure () -> Any?, _ error: Swift.Error) throws {
-        if assertion() != nil { throw error }
+    func assertNil(_ assertion: @autoclosure () throws -> Any?, _ error: Swift.Error) throws {
+        if try assertion() != nil { throw error }
     }
 
-    func assertNotNil(_ assertion: @autoclosure () -> Any?, _ error: Swift.Error) throws {
-        if assertion() == nil { throw error }
+    func assertNotNil(_ assertion: @autoclosure () throws -> Any?, _ error: Swift.Error) throws {
+        if try assertion() == nil { throw error }
     }
 
-    func assertTrue(_ assertion: @autoclosure () -> Bool, _ error: Swift.Error) throws {
-        if !assertion() { throw error }
+    func assertTrue(_ assertion: @autoclosure () throws -> Bool, _ error: Swift.Error) throws {
+        if try !assertion() { throw error }
     }
 
-    func assertFalse(_ assertion: @autoclosure () -> Bool, _ error: Swift.Error) throws {
+    func assertFalse(_ assertion: @autoclosure () throws -> Bool, _ error: Swift.Error) throws {
         try assertTrue(!assertion(), error)
     }
 
-    func assertEqual<T>(_ expression1: @autoclosure () -> T,
-                        _ expression2: @autoclosure () -> T,
+    func assertEqual<T>(_ expression1: @autoclosure () throws -> T,
+                        _ expression2: @autoclosure () throws -> T,
                         _ error: Swift.Error) throws where T: Equatable {
-        if expression1() != expression2() { throw error }
+        if try expression1() != expression2() { throw error }
     }
 
-    func assertNotEqual<T>(_ expression1: @autoclosure () -> T,
-                           _ expression2: @autoclosure () -> T,
+    func assertNotEqual<T>(_ expression1: @autoclosure () throws -> T,
+                           _ expression2: @autoclosure () throws -> T,
                            _ error: Swift.Error) throws where T: Equatable {
-        if expression1() == expression2() { throw error }
+        if try expression1() == expression2() { throw error }
     }
 
 }
