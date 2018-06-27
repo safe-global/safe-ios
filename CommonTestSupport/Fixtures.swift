22b4a5a4371ed0b729854f8f4330ee48d2a4fe9e
Andrey Scherbovich <andrey@gnosis.pm>
2018-06-27 14:28:33 +0200

    WA-449 call notificationService when pairing

---
+++ CommonTestSupport/Fixtures.swift
@@ -0,0 +1,19 @@
+//
+//  Copyright © 2018 Gnosis Ltd. All rights reserved.
+//
+
+import Foundation
+
+public struct BrowserExtensionFixture {
+
+    public static let testJSON = """
+            {"expirationDate": "2018-05-09T14:18:55+00:00",
+              "signature": {
+                "v": 27,
+                "r":"test",
+                "s":"me"
+              }
+            }
+            """
+
+}
