3b6e955a89352db0bfa860910ed68eee26d0a7e8
Andrey Scherbovich <andrey@gnosis.pm>
2018-09-03 14:51:35 +0200

    WA-533 Add manage tokens tests; subscribe on events to update ui;

--- Common/MockLogger.swift
+++ Common/MockLogger.swift
@@ -12,26 +12,32 @@ public class MockLogger: Logger {
     public var infoLogged = false
     public var debugLogged = false
 
+    public var loggedError: Error?
+
     public init() {}
 
     public func fatal(_ message: String, error: Error?, file: StaticString, line: UInt, function: StaticString) {
         print(file, function, line, message, error == nil ? "" : error!)
         fatalLogged = true
+        loggedError = error
     }
 
     public func error(_ message: String, error: Error?, file: StaticString, line: UInt, function: StaticString) {
         print(file, function, line, message, error == nil ? "" : error!)
         errorLogged = true
+        loggedError = error
     }
 
     public func info(_ message: String, error: Error?, file: StaticString, line: UInt, function: StaticString) {
-        infoLogged = true
         print(file, function, line, message, error == nil ? "" : error!)
+        infoLogged = true
+        loggedError = error
     }
 
     public func debug(_ message: String, error: Error?, file: StaticString, line: UInt, function: StaticString) {
-        debugLogged = true
         print(file, function, line, message, error == nil ? "" : error!)
+        debugLogged = true
+        loggedError = error
     }
 
 }
