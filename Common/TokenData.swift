9d76fbac2e5e8bf5d3379751814ff19e92171317
Andrey Scherbovich <andrey@gnosis.pm>
2018-09-10 14:01:47 +0200

    WA-536 Move TokensCell to SafeUIKit; remove unused images;

---
+++ Common/TokenData.swift
@@ -0,0 +1,30 @@
+//
+//  Copyright © 2018 Gnosis Ltd. All rights reserved.
+//
+
+import Foundation
+import BigInt
+
+public struct TokenData: Equatable, Hashable {
+
+    public let address: String
+    public let code: String
+    public let name: String
+    public let logoURL: URL?
+    public let decimals: Int
+    public let balance: BigInt?
+
+    public init(address: String, code: String, name: String, logoURL: String, decimals: Int, balance: BigInt?) {
+        self.address = address
+        self.code = code
+        self.name = name
+        self.logoURL = URL(string: logoURL)
+        self.decimals = decimals
+        self.balance = balance
+    }
+
+    public var hashValue: Int {
+        return address.hashValue
+    }
+
+}
