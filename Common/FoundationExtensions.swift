de5d0f4b7cd69af8ae7e7e81c6996583cc53b259
Andrey Scherbovich <andrey@gnosis.pm>
2018-11-08 16:06:41 +0100

    fix crash

--- Common/FoundationExtensions.swift
+++ Common/FoundationExtensions.swift
@@ -6,12 +6,28 @@ import Foundation
 
 public extension String {
 
-    var hasUppercaseLetter: Bool {
-        return rangeOfCharacter(from: CharacterSet.uppercaseLetters) != nil
+    var hasLetter: Bool {
+        return rangeOfCharacter(from: CharacterSet.letters) != nil
     }
 
     var hasDecimalDigit: Bool {
         return rangeOfCharacter(from: CharacterSet.decimalDigits) != nil
+    }
+
+    var hasNoTrippleChar: Bool {
+        guard count > 2 else { return true }
+        var current = self.first!
+        var longestSiquence = 1
+        for c in suffix(count - 1) {
+            if c == current {
+                longestSiquence += 1
+                guard longestSiquence < 3 else { return false }
+            } else {
+                current = c
+                longestSiquence = 1
+            }
+        }
+        return true
     }
 
 }
--- Common/FoundationExtensionsTests.swift
+++ Common/FoundationExtensionsTests.swift
@@ -7,34 +7,44 @@ import XCTest
 
 class FoundationExtensionsTests: XCTestCase {
 
-    func test_containsCapitalizedLetter() {
-        let empty = ""
-        let oneLowercase = "a"
-        let oneUppercase = "A"
-        let manyContainsOneUppercase = "abC"
-        let manyContainsNoneUppercase = "abc"
-        let manyContainsManyUppercase = "aBC"
-        XCTAssertFalse(empty.hasUppercaseLetter)
-        XCTAssertFalse(oneLowercase.hasUppercaseLetter)
-        XCTAssertTrue(oneUppercase.hasUppercaseLetter)
-        XCTAssertTrue(manyContainsOneUppercase.hasUppercaseLetter)
-        XCTAssertFalse(manyContainsNoneUppercase.hasUppercaseLetter)
-        XCTAssertTrue(manyContainsManyUppercase.hasUppercaseLetter)
+    let empty = ""
+    let noDigit = "a"
+    let oneDigit = "1"
+    let oneLowercase = "a"
+    let oneUppercase = "A"
+    let manyContainsNoneUppercase = "abc"
+    let manyContainsOneDigit = "ab1"
+    let manyContainsNoneDigit = "abc"
+    let manyContainsManyDigits = "a12"
+    let double = "aa"
+    let tripple = "aaa"
+    let trippleInTheMiddle = "1aaa4"
+    let trippleInTheEnd = "1aa4bbb"
+
+    func test_hasLetter() {
+        XCTAssertFalse(empty.hasLetter)
+        XCTAssertFalse(oneDigit.hasLetter)
+        XCTAssertTrue(oneLowercase.hasLetter)
+        XCTAssertTrue(oneUppercase.hasLetter)
+        XCTAssertTrue(manyContainsNoneUppercase.hasLetter)
     }
 
-    func test_containsDigit() {
-        let empty = ""
-        let noDigit = "a"
-        let oneDigit = "1"
-        let manyContainsOneDigit = "ab1"
-        let manyContainsNoneDigit = "abc"
-        let manyContainsManyDigits = "a12"
+    func test_hasDecimalDigit() {
         XCTAssertFalse(empty.hasDecimalDigit)
         XCTAssertFalse(noDigit.hasDecimalDigit)
         XCTAssertTrue(oneDigit.hasDecimalDigit)
         XCTAssertTrue(manyContainsOneDigit.hasDecimalDigit)
         XCTAssertFalse(manyContainsNoneDigit.hasDecimalDigit)
         XCTAssertTrue(manyContainsManyDigits.hasDecimalDigit)
+    }
+
+    func test_hasNoTrippleChar() {
+        XCTAssertTrue(empty.hasNoTrippleChar)
+        XCTAssertTrue(oneDigit.hasNoTrippleChar)
+        XCTAssertTrue(double.hasNoTrippleChar)
+        XCTAssertFalse(tripple.hasNoTrippleChar)
+        XCTAssertFalse(trippleInTheMiddle.hasNoTrippleChar)
+        XCTAssertFalse(trippleInTheEnd.hasNoTrippleChar)
     }
 
 }
