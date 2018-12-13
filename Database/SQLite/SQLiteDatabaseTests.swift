180fb8c87283e8eac4060b91976ae7facfd9b8db
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2018-12-13 14:39:03 +0100

    GHI-460 tests

--- Database/SQLite/SQLiteDatabaseTests.swift
+++ Database/SQLite/SQLiteDatabaseTests.swift
@@ -406,7 +406,7 @@ class SQLiteDatabaseTests: XCTestCase {
         XCTAssertNotNil(sqlite.bind_text_in_destructor)
 
         sqlite.bind_text_result = CSQLite3.SQLITE_MISUSE
-        let message = SQLiteDatabase.errorMessage(from: sqlite.open_result, sqlite, sqlite.open_pointer_result)
+        let message = SQLiteDatabase.errorMessage(from: CSQLite3.SQLITE_MISUSE, sqlite, sqlite.open_pointer_result)
         assertThrows(try stmt.set("text", at: 1), SQLiteDatabase.Error.failedToSetStatementParameter(message))
 
         sqlite.bind_text_result = CSQLite3.SQLITE_RANGE
