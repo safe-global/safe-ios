7c4d745604291a7d0a665c86d55b080b809fe0e9
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2019-02-08 12:33:08 +0100

    GH-533 integrated in application service

--- ReplaceBrowserExtensionFacade/FeeCalculationError.swift
+++ ReplaceBrowserExtensionFacade/FeeCalculationError.swift
@@ -11,14 +11,22 @@ public class FeeCalculationError: NSError {
     enum Description {
         static let insufficientBalance = LocalizedString("fee_calculation.error.insufficient_balance",
                                                          comment: "Insufficient funds.\nPlease add ETH to your Safe.")
+        static let extensionNotFound = LocalizedString("fee_calculation.error.extension_not_found",
+                                                       comment: "Browser extension is not connected.")
 
     }
     public enum Code: Int {
         case insufficientBalance
+        case extensionNotFound
     }
 
     public static let insufficientBalance =
         FeeCalculationError(domain: FeeCalculationError.domain,
                             code: Code.insufficientBalance.rawValue,
                             userInfo: [NSLocalizedDescriptionKey: Description.insufficientBalance])
+
+    public static let extensionNotFound =
+        FeeCalculationError(domain: FeeCalculationError.domain,
+                            code: Code.extensionNotFound.rawValue,
+                            userInfo: [NSLocalizedDescriptionKey: Description.extensionNotFound])
 }
