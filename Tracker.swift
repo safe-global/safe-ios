38cca3f2e80f2a1471d0f4122f07fc4de530945c
Andrey Scherbovich <andrey@gnosis.pm>
2019-02-27 18:13:17 +0100

    GHI-548 experiments with tracking

--- Tracker.swift
+++ Tracker.swift
@@ -7,9 +7,10 @@ import Foundation
 public protocol TrackingHandler {
     func track(view: TrackingView, contentId: String?, parameters: [String: Any]?)
     func track(event: TrackingEvent, view: TrackingView?, parameters: [String: Any]?)
+    func setScreenName(_ name: String, class: String?)
 }
 
-public class Tracker: TrackingHandler {
+public class Tracker {
 
     public static let shared = Tracker()
 
@@ -28,6 +29,13 @@ public class Tracker: TrackingHandler {
     public func track(event: TrackingEvent, view: TrackingView? = nil, parameters: [String: Any]? = nil) {
         for handler in trackingHandlers {
             handler.track(event: event, view: view, parameters: parameters)
+        }
+    }
+
+    public func setScreenName(_ name: String, class: AnyClass? = nil) {
+        for handler in trackingHandlers {
+            let className = `class` != nil ? String(describing: `class`!) : nil
+            handler.setScreenName(name, class: className)
         }
     }
 
