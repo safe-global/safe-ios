7f9189e3af60d223c02ef0f1dc3807d54ce57bd1
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2019-09-12 18:29:33 +0200

    Added keycard pairing and initialization step

---
+++ CommonImplementations/DBAbstractRepository.swift
@@ -0,0 +1,75 @@
+//
+//  Copyright © 2019 Gnosis Ltd. All rights reserved.
+//
+
+import Foundation
+import Database
+import Common
+
+open class DBAbstractRepository<T> {
+
+    // MARK: - Methods to override
+
+    open var table: TableSchema {
+        preconditionFailure()
+    }
+
+    open func insertionBindings(_ object: T) -> [SQLBindable?] {
+        preconditionFailure("Not implemented")
+    }
+
+    open func objectFromResultSet(_ rs: ResultSet) throws -> T? {
+        preconditionFailure("Not implemented")
+    }
+
+    open func primaryKeyBindings(_ item: T) -> [SQLBindable?] {
+        preconditionFailure()
+    }
+
+    // MARK: - Public Interface
+
+    public let db: Database
+
+    public init(db: Database) {
+        self.db = db
+    }
+
+    open func setUp() {
+        try! db.execute(sql: table.createTableSQL)
+    }
+
+    open func save(_ item: T) {
+        try! db.execute(sql: table.insertSQL, bindings: insertionBindings(item))
+    }
+
+    open func remove(_ item: T) {
+        try! db.execute(sql: table.deleteSQL, bindings: primaryKeyBindings(item))
+    }
+
+    open func findFirst() -> T? {
+        return try! first(of: db.execute(sql: table.findFirstSQL, resultMap: objectFromResultSet))
+    }
+
+    open func find(key: String, value: SQLBindable, orderBy: String) -> [T] {
+        return try! unwrapped(db.execute(sql: table.findSQL(key: key, orderBy: orderBy),
+                                         bindings: [value],
+                                         resultMap: objectFromResultSet))
+    }
+
+    open func all() -> [T] {
+        return try! unwrapped(db.execute(sql: table.findAllSQL, resultMap: objectFromResultSet))
+    }
+
+    open func first(of values: [T?]) -> T? {
+        return unwrapped(values).first
+    }
+
+    open func unwrapped(_ values: [T?]) -> [T] {
+        return values.compactMap { $0 }
+    }
+
+    open func bindable(_ values: [DBSerializable?]) -> [SQLBindable?] {
+        return values.map { $0?.serializedValue }
+    }
+
+}
--- CommonImplementations/DBEntityRepository.swift
+++ CommonImplementations/DBEntityRepository.swift
@@ -6,29 +6,7 @@ import Foundation
 import Database
 import Common
 
-open class DBEntityRepository<T: IdentifiableEntity<U>, U: BaseID> {
-
-    public let db: Database
-
-    public init(db: Database) {
-        self.db = db
-    }
-
-    open func setUp() {
-        try! db.execute(sql: table.createTableSQL)
-    }
-
-    open func save(_ object: T) {
-        try! db.execute(sql: table.insertSQL, bindings: insertionBindings(object))
-    }
-
-    open func remove(_ object: T) {
-        try! db.execute(sql: table.deleteSQL, bindings: primaryKeyBindings(object))
-    }
-
-    open func findFirst() -> T? {
-        return try! first(of: db.execute(sql: table.findFirstSQL, resultMap: objectFromResultSet))
-    }
+open class DBEntityRepository<T: IdentifiableEntity<U>, U: BaseID>: DBAbstractRepository<T> {
 
     open func find(id: U) -> T? {
         return try! first(of: db.execute(sql: table.findByPrimaryKeySQL,
@@ -36,47 +14,13 @@ open class DBEntityRepository<T: IdentifiableEntity<U>, U: BaseID> {
                                          resultMap: objectFromResultSet))
     }
 
-    open func find(key: String, value: SQLBindable, orderBy: String) -> [T] {
-        return try! unwrapped(db.execute(sql: table.findSQL(key: key, orderBy: orderBy),
-                                         bindings: [value],
-                                         resultMap: objectFromResultSet))
-    }
-
-    open func all() -> [T] {
-        return try! unwrapped(db.execute(sql: table.findAllSQL, resultMap: objectFromResultSet))
-    }
-
-    open func first(of values: [T?]) -> T? {
-        return unwrapped(values).first
-    }
-
-    open func unwrapped(_ values: [T?]) -> [T] {
-        return values.compactMap { $0 }
-    }
-
     open func nextID() -> U {
         return U()
     }
 
-    open func bindable(_ values: [DBSerializable?]) -> [SQLBindable?] {
-        return values.map { $0?.serializedValue }
-    }
-
-    // MARK: - Override these methods
-
-    open var table: TableSchema { preconditionFailure("Not implemented") }
-
-    open func insertionBindings(_ object: T) -> [SQLBindable?] {
-        preconditionFailure("Not implemented")
-    }
-
-    open func objectFromResultSet(_ rs: ResultSet) throws -> T? {
-        preconditionFailure("Not implemented")
-    }
-
     // MARK: Optional to override
 
-    open func primaryKeyBindings(_ object: T) -> [SQLBindable?] {
+    override open func primaryKeyBindings(_ object: T) -> [SQLBindable?] {
         return primaryKeyBindings(object.id)
     }
 
