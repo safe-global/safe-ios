ffcc7767062f6e51acfa350445201a2edb2ae11f
Andrey Scherbovich <andrey@gnosis.pm>
2019-09-24 11:49:06 +0200

    GH-1088 Add lost unit tests; Add WalletData; Bootstrap Select Safes command, coordinator, and view controller

---
+++ Common/WalletData.swift
@@ -0,0 +1,24 @@
+//
+//  Copyright © 2019 Gnosis Ltd. All rights reserved.
+//
+
+import Foundation
+
+public struct WalletData {
+
+    public enum State {
+        case pending
+        case created
+    }
+
+    public let address: String
+    public let name: String
+    public let state: State
+
+    public init(address: String, name: String, state: State) {
+        self.address = address
+        self.name = name
+        self.state = state
+    }
+
+}
