f01b110330a2b38fc587cbdfc5cd763a2564a822
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2020-04-16 13:52:46 +0000

    feat: basic project setup (#1)
    * feat: added tab icons and launch screen
    
    - Restricted orientation to portrait
    
    * Added text styles and colors
    
    * Restyled UI and library
    
    * deployment target to 13.0
    
    * target to iphone
    
    * whitespaces

--- Multisig/AppDelegate.swift
+++ Multisig/AppDelegate.swift
@@ -11,9 +11,6 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -77,6 +74,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
 }
-
