b2ad6c87aab88dcc5174002f099b71da47439983
Mouaz <Mouaz.alzahabi@gnosis.pm>
2020-04-22 10:37:27 +0200

    Create VerifiableTextField

--- Multisig/UI/Safe Management/EnterSafeNameView.swift
+++ Multisig/UI/Safe Management/EnterSafeNameView.swift
@@ -9,13 +9,72 @@
 import SwiftUI
 
 struct EnterSafeNameView: View {
+    var address: String? = "0xAB3e244863e1a127333aBa15235aD50E0954146F"
+    @State private var name: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 0) {
+                Identicon(address!).frame(width: 40, height: 40, alignment: .center)
+                HStack (alignment: .top) {
+                    Image("ico-circle-check")
+                    AddressText(address!).multilineTextAlignment(.center)
+                }
+                .padding(.trailing, 37)
+                .padding(.top, 9)
+                Text("Choose a name for the Safe. The name is only stored locally and will not be shared with Gnosis or any third parties.")
+                    
+                    .font(Font.gnoBody.weight(.medium))
+                    .padding(.top, 27)
+                    .padding(.bottom, 24)
+                    .multilineTextAlignment(.center)
+
+                VerifiableTextField(name: "Enter Name", validator: { input in
+                    return input.isEmpty ? "Safe name can't be empty" : ""
+                })
+                
+                
+                Spacer()
+            }
+            .foregroundColor(.gnoDarkBlue)
+            .padding()
+            .navigationBarTitle(title, displayMode: .inline)
+            .navigationBarItems(leading: backButton, trailing: nextButton)
+        }
+    }
+    
+    var title: Text {
+        Text("Load Safe Multisig")
+            .font(Font.gnoBody.weight(.semibold))
+            .foregroundColor(.gnoDarkBlue)
+    }
+    
+    @Environment(\.presentationMode)
+    var presentationMode: Binding<PresentationMode>
+    var backButton: some View {
+        Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+        }, label: {
+            HStack(spacing: 4) {
+                Image(systemName: "chevron.left")
+                Text("Back")
+            }
+        })
+        .accentColor(.gnoHold)
+    }
+
+    var nextButton: some View {
+        Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+        }, label: {
+            Text("Next")
+            .fontWeight(.semibold)
+        })
+        .accentColor(.gnoHold)
     }
 }
 
 struct EnterSafeNameView_Previews: PreviewProvider {
     static var previews: some View {
-        EnterSafeNameView()
+        EnterSafeNameView(address: "0xAB3e244863e1a127333aBa15235aD50E0954146F")
     }
 }
---
+++ Multisig/UI/UI Library/VerifiableTextField.swift
@@ -0,0 +1,46 @@
+//
+//  VerifiableTextField.swift
+//  Multisig
+//
+//  Created by Moaaz on 4/20/20.
+//  Copyright © 2020 Gnosis Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct VerifiableTextField: View {
+    var name: String
+    @State private var text: String = ""
+    var validator: (String) -> String
+    var body: some View {
+        let errorMessage = validator(text)
+        var foregroundColor = Color.gnoDarkGrey
+        var borderColor = Color.gnoWhitesmoke
+        
+        if !errorMessage.isEmpty {
+            foregroundColor = Color.gnoTomato
+            borderColor = Color.gnoTomato
+        }
+        
+        return VStack (alignment: .leading) {
+            HStack {
+                TextField(name, text: $text).foregroundColor(foregroundColor).frame(height: 56)
+                .font(.gnoBody)
+                if errorMessage.isEmpty {
+                    Image("ico-circle-check")
+                }
+            }.padding().background(RoundedRectangle(cornerRadius: 10)
+            .strokeBorder(borderColor, lineWidth: 2).frame(height: 56))
+            
+            Text(errorMessage).font(.gnoCallout).foregroundColor(Color.gnoTomato).frame(alignment: .leading)
+        }
+    }
+}
+
+struct VerifiableTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        VerifiableTextField(name: "Enter name") { input in
+            return "input can't be empty"
+        }
+    }
+}
