92b77c1a39c6303c4e2f94107edf6c0d81352eb9
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2020-04-27 10:53:15 +0200

    removed not used files

--- Multisig/Logic/Models/Safe.swift
+++ Multisig/Logic/Models/Safe.swift
@@ -34,33 +34,4 @@ extension Safe: Identifiable {
         return request
     }
 
-    static func exists(at address: Address) throws -> Bool {
-        let stringAddress = address.hex(eip55: true)
-        do {
-            _ = try App.shared.safeRelayService.safeInfo(at: stringAddress)
-            return true
-        } catch let HTTPClient.Error.networkRequestFailed(request, response, data) {
-            if (response as? HTTPURLResponse)?.statusCode == 404 {
-                return false
-            }
-
-            struct BackendException: Codable {
-                var exception: String
-
-                var isSafeNotDeployed: Bool {
-                    exception.starts(with: "SafeNotDeployed:")
-                }
-            }
-
-            if let data = data,
-                let exception = try? JSONDecoder().decode(
-                    BackendException.self, from: data
-                ),
-                exception.isSafeNotDeployed {
-                return false
-            }
-
-            throw HTTPClient.Error.networkRequestFailed(request, response, data)
-        }
-    }
 }
