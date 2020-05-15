99133197a898a934fb254c39652a25f29e8b7177
Mouaz <Mouaz.alzahabi@gnosis.pm>
2020-05-15 11:31:44 +0200

    add AdvancedAppSettingsView

---
+++ Multisig/UI/Settings/AdvancedAppSettingsView.swift
@@ -0,0 +1,21 @@
+//
+//  AdvancedAppSettingsView.swift
+//  Multisig
+//
+//  Created by Moaaz on 5/14/20.
+//  Copyright © 2020 Gnosis Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct AdvancedAppSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AdvancedAppSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AdvancedAppSettingsView()
+    }
+}
