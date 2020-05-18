9d49edd30f135629872a45fce66b2825ba4d9d6e
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2020-05-18 11:52:51 +0200

    gh-71  changed the edit name screen

---
+++ Multisig/UI/UI Library/BackButton.swift
@@ -0,0 +1,37 @@
+//
+//  BackButton.swift
+//  Multisig
+//
+//  Created by Dmitry Bespalov on 18.05.20.
+//  Copyright © 2020 Gnosis Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct BackButton: View {
+
+    @Environment(\.presentationMode)
+    var presentationMode: Binding<PresentationMode>
+
+    var label: String
+
+    init(_ label: String) {
+        self.label = label
+    }
+
+    var body: some View {
+        Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+        }) {
+            Image(systemName: "chevron.left").font(.system(size: 25))
+            Text(label)
+        }
+        .offset(x: -7)
+    }
+}
+
+struct BackButton_Previews: PreviewProvider {
+    static var previews: some View {
+        BackButton("Cancel")
+    }
+}
