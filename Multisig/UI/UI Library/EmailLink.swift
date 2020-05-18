53dc2e04c57ab21243bafb845a51de7d5c109eaa
Mouaz <Mouaz.alzahabi@gnosis.pm>
2020-05-18 13:10:04 +0200

    gh-90 create EmailLink&rename BrowseLinkButton

---
+++ Multisig/UI/UI Library/EmailLink.swift
@@ -0,0 +1,57 @@
+//
+//  EmailLinkButton.swift
+//  Multisig
+//
+//  Created by Moaaz on 5/18/20.
+//  Copyright © 2020 Gnosis Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmailLink: View {
+    @State
+    private var showsLink: Bool = false
+
+    @State
+    var error: IdentifiableByHash<String>?
+
+    var title: String
+    var url: URL
+    var iconName: String?
+
+    var body: some View {
+        Button(action: {
+            if EmailSupportViewController.isAvailable {
+                self.showsLink.toggle()
+            } else {
+                self.error = IdentifiableByHash("Mail is not configured.")
+            }
+        }) {
+            HStack {
+                if iconName != nil {
+                    Image(iconName!)
+                }
+
+                BodyText(title)
+                Spacer()
+                Image(systemName: "chevron.right")
+                    .font(Font.footnote.bold())
+                    .foregroundColor(Color.systemGray6Light)
+            }
+        }
+        .frame(height: 44)
+        .alert(item: $error) { msg in
+            Alert(title: Text(msg.value))
+        }
+        .sheet(isPresented: $showsLink, content: {
+            EmailSupportViewController(url: self.url)
+        })
+
+    }
+}
+
+struct EmailLink_Previews: PreviewProvider {
+    static var previews: some View {
+        EmailLink(title: "Email", url: URL(string: "safe@gnosis.io")!)
+    }
+}
