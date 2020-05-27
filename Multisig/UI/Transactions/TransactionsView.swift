31cc2f74bd319f4d09a2312f2ecd197befb0ab0b
Mouaz <Mouaz.alzahabi@gnosis.pm>
2020-05-27 11:48:15 +0200

    gh-189 Create TitleText, EmptyListPlaceholder, TransactionsView

---
+++ Multisig/UI/Transactions/TransactionsView.swift
@@ -0,0 +1,36 @@
+//
+//  TransactionsView.swift
+//  Multisig
+//
+//  Created by Moaaz on 5/27/20.
+//  Copyright © 2020 Gnosis Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct TransactionsView: View {
+
+    @FetchRequest(fetchRequest: Safe.fetchRequest().selected())
+    var selectedSafe: FetchedResults<Safe>
+
+    var body: some View {
+        let safe = selectedSafe.first
+        return ZStack {
+            Rectangle()
+               .edgesIgnoringSafeArea(.all)
+               .foregroundColor(Color.gnoWhite)
+            if safe == nil {
+                AddSafeIntroView()
+            }
+            else {
+                EmptyListPlaceholder(label: "Transactions will appear here", image: "ico-no-transactions")
+            }
+        }
+    }
+}
+
+struct TransactionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TransactionsView()
+    }
+}
