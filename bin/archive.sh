004e23d868ccf5c4adfa7333bbd2e8fbe664aefc
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2020-06-02 10:24:59 +0000

    gh-81 ci (#198)
    * gh-81  setup fastlane
    
    * gh-81  updated script
    
    * gh-81  updated path
    
    * gh-81  modified path
    
    * gh-81  debug ruby
    
    * gh-81  updated wit hruby version
    
    * gh-81  more vars
    
    * gh-81  even more vars
    
    * gh-81  shims path
    
    * gh-81  rbenv which
    
    * gh-81  gems
    
    * gh-81  cp example
    
    * gh-81  xcargs
    
    * gh-81  auto code sign
    
    * gh-81  xcargs
    
    * gh-81  xcargs
    
    * gh-81  upd build command
    
    * gh-81  jenkknsfile
    
    * gh-81  updated build script
    
    * gh-81  update build params
    
    * gh-81  added build scripts
    
    * gh-81  updated path
    
    * gh-81  updated path
    
    * gh-81  junit results
    
    * gh-81  color logs
    
    * gh-81  enabled code coverage
    
    * gh-81  update gemfile
    
    * gh-81  updated lock file
    
    * gh-81  remove test log
    
    * gh-81  update regexp
    
    * gh-81  debug branch name
    
    * gh-81  updated branch condition

---
+++ bin/archive.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+XCODE_SCHEME=$1
+OUTPUT_DIR="Build/$XCODE_SCHEME"
+ARCHIVE_PATH="$OUTPUT_DIR/Multisig.xcarchive"
+
+mkdir -p "$OUTPUT_DIR"
+
+bin/configure.sh
+
+xcrun agvtool new-version -all $BUILD_NUMBER
+
+set -o pipefail && \
+xcrun xcodebuild archive \
+    -workspace Multisig.xcworkspace \
+    -scheme "$XCODE_SCHEME" \
+    -destination "generic/platform=iOS" \
+    -archivePath "$ARCHIVE_PATH" \
+    -allowProvisioningUpdates \
+| tee "$OUTPUT_DIR/xcodebuild-archive.log" | xcpretty -c
+
+set -o pipefail && \
+xcrun xcodebuild -exportArchive \
+    -archivePath "$ARCHIVE_PATH" \
+    -exportPath Build \
+    -exportOptionsPlist Multisig/ExportOptions.plist \
+    -allowProvisioningUpdates \
+| tee "$OUTPUT_DIR/xcodebuild-export.log" | xcpretty -c
