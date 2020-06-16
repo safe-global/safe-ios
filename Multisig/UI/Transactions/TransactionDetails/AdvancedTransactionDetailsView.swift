7801e848c6ca87c4f431be074d39a967a99a0ebc
Andrey Scherbovich <andrey@gnosis.io>
2020-06-16 13:24:18 +0200

    gh-95 add tracking for transaction details and advanced transaction details

--- Multisig/UI/Transactions/TransactionDetails/AdvancedTransactionDetailsView.swift
+++ Multisig/UI/Transactions/TransactionDetails/AdvancedTransactionDetailsView.swift
@@ -12,6 +12,8 @@ struct AdvancedTransactionDetailsView: View {
     let transaction: BaseTransactionViewModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!").onAppear {
+            self.trackEvent(.transactionsDetailsAdvanced)
+        }
     }
 }
