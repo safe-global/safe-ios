1361a68b876c46e1f575b8ed5cf7fda0eabd2a67
Mouaz <Mouaz.alzahabi@gnosis.pm>
2020-06-18 11:55:44 +0200

    gh-251 Display executed, confirmations

--- Multisig/UI/Transactions/TransactionConfirmationsView/TransactionConfirmationsView.swift
+++ Multisig/UI/Transactions/TransactionConfirmationsView/TransactionConfirmationsView.swift
@@ -12,21 +12,22 @@ struct TransactionConfirmationsView: View {
     let transaction: TransactionViewModel
     let safe: Safe
 
-    #warning("TODO: update after clarified requirements")
-
     var body: some View {
-        assert(transaction.hasConfirmations)
-        return VStack(alignment: .leading, spacing: 1) {
-            TransactionConfirmationCell(address: transaction.confirmations!.first!.address, style: .created)
+        VStack(alignment: .leading, spacing: 1) {
+            TransactionConfiramtionStatusView(style: .created)
+            VerticalBarView()
 
             ForEach(transaction.confirmations ?? [], id: \.address) { confirmation in
                 TransactionConfirmationCell(address: confirmation.address, style: .confirmed)
             }
 
             if transaction.status == .success {
-                TransactionConfirmationCell(address: transaction.confirmations!.last!.address, style: .executed)
-            }
-            else {
+                if transaction.executor != nil {
+                    TransactionConfirmationCell(address: transaction.executor!, style: .executed)
+                } else {
+                    TransactionConfiramtionStatusView(style: .executed)
+                }
+            } else {
                 actionsView
             }
         }
@@ -39,7 +40,7 @@ struct TransactionConfirmationsView: View {
             }
 
             if transaction.status == .waitingConfirmation {
-                if !isConfirmedBySelectedSafe {
+                if !isConfirmed {
                     VerticalBarView()
                 }
 
@@ -55,7 +56,7 @@ struct TransactionConfirmationsView: View {
         } else if transaction.status == .failed {
             return .failed
         } else if status == .waitingConfirmation {
-            return isConfirmedBySelectedSafe ? nil : .confirm
+            return isConfirmed ? nil : .confirm
         } else if status == .waitingExecution {
             return .execute
         }
@@ -63,9 +64,7 @@ struct TransactionConfirmationsView: View {
         return nil
     }
 
-    var isConfirmedBySelectedSafe: Bool {
-        transaction.confirmations!.first { (confirmation) -> Bool in
-            confirmation.address == safe.address
-        } != nil
+    var isConfirmed: Bool {
+        !transaction.confirmations!.isEmpty
     }
 }
