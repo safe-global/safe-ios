ab27510ae5545607181f31ba8cef53423feb8df1
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2020-07-23 10:30:13 +0200

    gh-31  using snackbar for tooltip

--- Multisig/UI/UI Library/Buttons/CopyButton.swift
+++ Multisig/UI/UI Library/Buttons/CopyButton.swift
@@ -15,33 +15,12 @@ struct CopyButton<Content: View>: View {
     var value: String?
     var content: Content
 
-    @State private var wasCopied: Bool = false
-    @State private var timerID: UUID?
-    private let tooltipDuration: TimeInterval = 2
-    private let tooltipText = "Copied to clipboard"
-    private let tooltipHeight: CGFloat = 48
-
     init(_ value: String? = nil, @ViewBuilder _ content: () -> Content) {
         self.value = value
         self.content = content()
     }
 
     var body: some View {
-        ZStack {
-            copyButton
-                .background(wasCopied ? Color.gnoSystemSelection : nil)
-
-//            Tooltip(tooltipText)
-//                .offset(y: -tooltipHeight)
-//                .opacity(wasCopied ? 1 : 0)
-//                .zIndex(10_000)
-
-            Tooltip(tooltipText)
-                .opacity(wasCopied ? 1 : 0)
-        }
-    }
-
-    var copyButton: some View {
         Button(action: copyToPasteboard) {
             content
         }
@@ -50,28 +29,7 @@ struct CopyButton<Content: View>: View {
 
     func copyToPasteboard() {
         Pasteboard.string = value
-
-        // Alternative: snackbar
-//        App.shared.snackbar.show(message: "Copied to clipboard")
-
-        if let existing = timerID, let timer = timers[existing] {
-            timer.invalidate()
-            timers.removeValue(forKey: existing)
-        }
-
-        withAnimation {
-            wasCopied = true
-        }
-
-        let id = UUID()
-        timerID = id
-        let timer = Timer.scheduledTimer(withTimeInterval: tooltipDuration, repeats: false) { _ in
-            withAnimation {
-                self.wasCopied = false
-            }
-            timers.removeValue(forKey: id)
-        }
-        timers[id] = timer
+        App.shared.snackbar.show(message: "Copied to clipboard", duration: 2)
     }
 }
 
