443f2d8c9e3a97f90135d69e1123ca1261fe14b2
Mouaz <Mouaz.alzahabi@gnosis.pm>
2020-10-13 10:42:45 +0200

    gh-587 Trim spaces

--- Multisig/Logic/Models/PrivateKey/web3swift/String+Extension.swift
+++ Multisig/Logic/Models/PrivateKey/web3swift/String+Extension.swift
@@ -55,4 +55,9 @@ extension String {
         }
         return self
     }
+
+    func condenseWhitespace() -> String {
+        let components = self.components(separatedBy: NSCharacterSet.whitespacesAndNewlines)
+        return components.filter { !$0.isEmpty }.joined(separator: " ")
+    }
 }
