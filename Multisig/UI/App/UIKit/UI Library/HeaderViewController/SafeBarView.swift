9ae78a924d50191f893e3d6aa38b11675cacc0f3
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2020-10-29 19:22:18 +0100

    gh-665 added comment

--- Multisig/UI/App/UIKit/UI Library/HeaderViewController/SafeBarView.swift
+++ Multisig/UI/App/UIKit/UI Library/HeaderViewController/SafeBarView.swift
@@ -22,9 +22,7 @@ class SafeBarView: UINibView {
         textLabel.setStyle(.headline)
         detailLabel.setStyle(GNOTextStyle.body.color(.gnoMediumGrey))
         addTarget(self, action: #selector(didTouchDown(sender:forEvent:)), for: .touchDown)
-        addTarget(self, action: #selector(didTouchUp(sender:forEvent:)), for: .touchUpInside)
-        addTarget(self, action: #selector(didTouchUp(sender:forEvent:)), for: .touchCancel)
-        addTarget(self, action: #selector(didTouchUp(sender:forEvent:)), for: .touchUpOutside)
+        addTarget(self, action: #selector(didTouchUp(sender:forEvent:)), for: [.touchUpInside, .touchUpOutside, .touchCancel])
     }
 
     func setAddress(_ value: Address) {
@@ -36,6 +34,7 @@ class SafeBarView: UINibView {
         textLabel.text = value
     }
 
+    // encapsulating the button's target-action API
     func addTarget(_ target: Any?, action: Selector, for controlEvents: UIControl.Event) {
         button.addTarget(target, action: action, for: controlEvents)
     }
@@ -44,6 +43,7 @@ class SafeBarView: UINibView {
         button.removeTarget(target, action: action, for: controlEvents)
     }
 
+    // visual reaction for user touches
     @objc func didTouchDown(sender: UIButton, forEvent event: UIEvent) {
         alpha = 0.7
     }
