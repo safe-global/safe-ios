eee04f4e9b812626a89148ec5ffb5ee66cfd89c4
Andrey Scherbovich <andrey@gnosis.io>
2020-12-03 07:20:05 +0100

    gh-736 add DetailAccountAndTextCell

--- Multisig/UI/App/UIKit/UI Library/ReusableCells/DetailAccountAndTextCell.swift
+++ Multisig/UI/App/UIKit/UI Library/ReusableCells/DetailAccountAndTextCell.swift
@@ -1,25 +1,29 @@
 //
-//  DetailAccountCell.swift
+//  DetailAccountAndTextCell.swift
 //  Multisig
 //
-//  Created by Andrey Scherbovich on 02.12.20.
+//  Created by Andrey Scherbovich on 03.12.20.
 //  Copyright © 2020 Gnosis Ltd. All rights reserved.
 //
 
 import UIKit
 
-class DetailAccountCell: UITableViewCell {
+class DetailAccountAndTextCell: UITableViewCell {
     var onViewDetails: (() -> Void)?
 
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var addressInfoView: AddressInfoView!
     @IBOutlet private weak var titleTopConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var textTitleLabel: UILabel!
+    @IBOutlet private weak var textDetailsLabel: UILabel!
 
     private let titleTopSpace: CGFloat = 16
 
     override func awakeFromNib() {
         super.awakeFromNib()
         titleLabel.setStyle(.headline)
+        textTitleLabel.setStyle(.body)
+        textDetailsLabel.setStyle(GNOTextStyle.body.color(.gnoDarkGrey))
         addressInfoView.setDetailImage(#imageLiteral(resourceName: "ico-browse-address"))
         addressInfoView.onDisclosureButtonAction = viewDetails
     }
@@ -28,6 +32,11 @@ class DetailAccountCell: UITableViewCell {
         titleLabel.text = title
         titleTopConstraint.constant = title == nil ? 0 : titleTopSpace
         addressInfoView.setAddressInfo(addressInfo)
+    }
+
+    func setText(title: String, details: String) {
+        textTitleLabel.text = title
+        textDetailsLabel.text = details
     }
 
     private func viewDetails() {
