07398ee27e460364c7c3804dfe96aae1d8c73592
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2020-12-18 14:00:35 +0100

    gh-366 Displaying sag in list

--- Multisig/UI/App/UIKit/Assets/CollectiblesViewController/CollectibleTableViewCell.swift
+++ Multisig/UI/App/UIKit/Assets/CollectiblesViewController/CollectibleTableViewCell.swift
@@ -7,16 +7,25 @@
 //
 
 import UIKit
+import WebKit
 
 class CollectibleTableViewCell: UITableViewCell {
     @IBOutlet private weak var cellImageView: UIImageView!
     @IBOutlet private weak var cellNameLabel: UILabel!
     @IBOutlet private weak var cellDescriptionLabel: UILabel!
+    @IBOutlet private weak var webView: WKWebView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         cellNameLabel.setStyle(.headline)
         cellDescriptionLabel.setStyle(.body)
+        webView.navigationDelegate = self
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        webView.stopLoading()
+        webView.isHidden = true
     }
 
     func setName(_ value: String) {
@@ -28,10 +37,34 @@ class CollectibleTableViewCell: UITableViewCell {
     }
 
     func setImage(with URL: URL?, placeholder: UIImage) {
+        webView.isHidden = true
         if let url = URL {
-            cellImageView.kf.setImage(with: url, placeholder: placeholder)
+            if url.pathExtension.caseInsensitiveCompare("svg") == .orderedSame {
+                let html = """
+                <html>
+                <head>
+                <meta name="viewport" content="width=device-width, shrink-to-fit=YES"/>
+                </head>
+                <body><img src="\(url)"/></body>
+                </html>
+                """
+                webView.loadHTMLString(html, baseURL: nil)
+                cellImageView.image = placeholder
+            } else {
+                cellImageView.kf.setImage(with: url, placeholder: placeholder)
+            }
         } else {
             cellImageView.image = placeholder
+        }
+    }
+}
+
+extension CollectibleTableViewCell: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        webView.alpha = 0
+        webView.isHidden = false
+        UIView.animate(withDuration: 0.25) {
+            webView.alpha = 1
         }
     }
 }
