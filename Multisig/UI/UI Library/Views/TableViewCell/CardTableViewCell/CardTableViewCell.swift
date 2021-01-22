990203ae2ded6b6600f94c6a8232beb9fbf557af
Mouaz <Mouaz.alzahabi@gnosis.pm>
2021-01-22 19:55:22 +0100

    gh-864 Fix comments

--- Multisig/UI/UI Library/Views/TableViewCell/CardTableViewCell/CardTableViewCell.swift
+++ Multisig/UI/UI Library/Views/TableViewCell/CardTableViewCell/CardTableViewCell.swift
@@ -10,22 +10,23 @@ import UIKit
 
 class CardTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var bodyLabel: UILabel!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var iconImageView: UIImageView!
-    @IBOutlet weak var containerView: UIView!
+    @IBOutlet private weak var bodyLabel: UILabel!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var iconImageView: UIImageView!
+    @IBOutlet private weak var containerView: UIView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        containerView.layer.cornerRadius = 20.0
-        containerView.layer.shadowColor = UIColor.gnoShadow.cgColor
+        containerView.layer.cornerRadius = 10.0
+        containerView.layer.shadowColor = UIColor.gnoCardShadow.cgColor
         containerView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
         containerView.layer.shadowRadius = 12.0
         containerView.layer.shadowOpacity = 0.7
 
         titleLabel.setStyle(.headline)
         bodyLabel.setStyle(.body)
+        selectionStyle = .none
     }
 
     func set(image: UIImage?) {
