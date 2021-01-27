dfd5d7c2c1b3e4ec0533f05febc75ccdf8bec017
Andrey Scherbovich <andrey@gnosis.io>
2021-01-27 16:45:09 +0700

    gh-808 fix

--- Multisig/UI/Safe Management/Add Safe/EnterSafeAddressViewController/EnterSafeAddressViewController.swift
+++ Multisig/UI/Safe Management/Add Safe/EnterSafeAddressViewController/EnterSafeAddressViewController.swift
@@ -145,7 +145,8 @@ class EnterSafeAddressViewController: UIViewController {
                                                         error: GSError.InvalidSafeAddress()).localizedDescription
                             self.addressField.setError(message)
                         } else {
-                            self.addressField.setError(error)
+                            let message = GSError.error(description: "Can’t use this address", error: error)
+                            self.addressField.setError(message)
                         }
                     }
                 case .success(let info):
