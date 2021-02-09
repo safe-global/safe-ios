d1c9d8045f66507cb3f1358b91ad17b7bdcc3ef6
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2021-02-09 16:52:59 +0100

    gh-882 updated pod

--- Pods/idn2Swift/unistring/include/uniwidth.h
+++ Pods/idn2Swift/unistring/include/uniwidth.h
@@ -1,22 +1,22 @@
 /* DO NOT EDIT! GENERATED AUTOMATICALLY! */
 /* Display width functions.
-   Copyright (C) 2001-2002, 2005, 2007, 2009-2020 Free Software Foundation,
+   Copyright (C) 2001-2002, 2005, 2007, 2009-2021 Free Software Foundation,
    Inc.
 
-This program is free software: you can redistribute it and/or
-modify it under the terms of either:
+   This program is free software: you can redistribute it and/or
+   modify it under the terms of either:
 
-* the GNU Lesser General Public License as published by the Free
-Software Foundation; either version 3 of the License, or (at your
-option) any later version.
+     * the GNU Lesser General Public License as published by the Free
+       Software Foundation; either version 3 of the License, or (at your
+       option) any later version.
 
-or
+   or
 
-* the GNU General Public License as published by the Free
-Software Foundation; either version 2 of the License, or (at your
-option) any later version.
+     * the GNU General Public License as published by the Free
+       Software Foundation; either version 2 of the License, or (at your
+       option) any later version.
 
-or both in parallel, as here.
+   or both in parallel, as here.
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
