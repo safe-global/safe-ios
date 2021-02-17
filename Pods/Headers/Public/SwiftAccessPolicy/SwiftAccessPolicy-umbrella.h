ddffb91bd5af288767c47d625240d76d7f6baa54
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2021-02-17 12:31:58 +0100

    gh-941 Added access policy pod

---
+++ Pods/Headers/Public/SwiftAccessPolicy/SwiftAccessPolicy-umbrella.h
@@ -0,0 +1,1 @@
+../../../Target Support Files/SwiftAccessPolicy/SwiftAccessPolicy-umbrella.h
---
+++ Pods/SwiftAccessPolicy/SwiftAccessPolicy/User.swift
@@ -0,0 +1,47 @@
+//
+//  User.swift
+//  SwiftAccessPolicy
+//
+//  Created by Andrey Scherbovich on 25.03.20.
+//  Copyright © 2020 Gnosis Ltd. All rights reserved.
+//
+
+import Foundation
+
+public struct User {
+    public let id: UUID
+    public private(set) var encryptedPassword: String
+    public private(set) var sessionRenewedAt: Date?
+    public private(set) var failedAuthAttempts: Int = 0
+    public private(set) var accessBlockedAt: Date?
+
+    public init(userID: UUID, encryptedPassword: String) {
+        self.id = userID
+        self.encryptedPassword = encryptedPassword
+    }
+
+    mutating func updatePassword(encryptedPassword: String) {
+        self.encryptedPassword = encryptedPassword
+    }
+
+    mutating func renewSession(at time: Date) {
+        self.sessionRenewedAt = time
+        self.failedAuthAttempts = 0
+        self.accessBlockedAt = nil
+    }
+
+    mutating func denyAccess() {
+        self.sessionRenewedAt = nil
+        self.failedAuthAttempts += 1
+    }
+
+    mutating func blockAccess(at time: Date) {        
+        self.accessBlockedAt = time
+    }
+
+    mutating func logout() {
+        self.sessionRenewedAt = nil
+        self.failedAuthAttempts = 0
+        self.accessBlockedAt = nil
+    }
+}
