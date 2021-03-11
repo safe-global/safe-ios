ed6ba8877758036c70b354fd5c71efe0e6faf46f
Mouaz <Mouaz.alzahabi@gnosis.pm>
2021-03-11 14:02:26 +0100

    GH-938 Create screen

---
+++ Multisig/UI/Settings/OwnerKeyManagement/ChooseOwnerKeyViewController/ChooseOwnerKeyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChooseOwnerKeyViewController.swift
+//  Multisig
+//
+//  Created by Moaaz on 3/10/21.
+//  Copyright © 2021 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ChooseOwnerKeyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
