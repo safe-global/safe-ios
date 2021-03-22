ac63e0557e8246ccf81648b937d50f95bafda6ca
Mouaz <Mouaz.alzahabi@gnosis.pm>
2021-03-22 18:05:44 +0100

    GH-1023 Hide help link in history tx

--- Multisig/UI/Transaction/TransactionDetailsViewController/Transaction Detail Cells/DetailRejectionInfoCell.swift
+++ Multisig/UI/Transaction/TransactionDetailsViewController/Transaction Detail Cells/DetailRejectionInfoCell.swift
@@ -12,6 +12,7 @@ class DetailRejectionInfoCell: UITableViewCell, ExternalURLSource {
     @IBOutlet private weak var descriptionLabel: UILabel!
     @IBOutlet private weak var linkLabel: UILabel!
     @IBOutlet private weak var linkButton: UIButton!
+    @IBOutlet private weak var helpLinkContinerView: UIView!
 
     private(set) var url: URL? = App.configuration.help.payForCancellationURL
 
@@ -21,13 +22,15 @@ class DetailRejectionInfoCell: UITableViewCell, ExternalURLSource {
         linkLabel.hyperLinkLabel(linkText: "Why do I need to pay for rejecting a transaction?")
     }
 
-    func setNonce(_ nonce: UInt256?) {
+    func setNonce(_ nonce: UInt256?, showHelpLink: Bool = true) {
         let base = "This is an on-chain rejection that doesn’t send any funds. Executing this on-chain rejection will replace all currently awaiting transactions"
         if let nonce = nonce {
             descriptionLabel.text = base + " with nonce \(nonce)."
         } else {
             descriptionLabel.text = base + " with the same nonce."
         }
+
+        helpLinkContinerView.isHidden = !showHelpLink
     }
     
     @IBAction func linkButtonTouched(_ sender: Any) {
