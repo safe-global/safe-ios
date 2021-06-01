523714adcfab219d05b9b208729cc0653b413451
Mouaz <Mouaz.alzahabi@gnosis.pm>
2021-06-01 17:25:11 +0200

    Fix on-chain-rejection label trimming + clickable arrow

--- Multisig/UI/Settings/OwnerKeyManagement/OwnerKeysListViewController/OwnerKeysListTableViewCell.swift
+++ Multisig/UI/Settings/OwnerKeyManagement/OwnerKeysListViewController/OwnerKeysListTableViewCell.swift
@@ -16,7 +16,7 @@ class OwnerKeysListTableViewCell: UITableViewCell {
 
         addressInfoView.setDetailImage(nil)
         addressInfoView.copyEnabled = false
-        addressInfoView.setDetailImage(UIImage(named: "arrow"))
+        addressInfoView.setDetailImage(nil)
     }
 
     func set(address: Address, title: String) {
