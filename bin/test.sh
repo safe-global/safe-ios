624dfe277055ebc5f16a8d8b82f733bc1977f5c5
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2021-07-12 14:29:42 +0200

    gh-1234 Core-data related intermittent error gone

--- bin/test.sh
+++ bin/test.sh
@@ -14,11 +14,11 @@ bin/configure.sh
 
 ############
 # uncomment when you need to delete the app from the simulator
-DEVICE="iPhone 11 Pro"
-APP_ID="io.gnosis.multisig.staging.rinkeby"
-set +e && xcrun simctl boot "${DEVICE}" 2>/dev/null
-xcrun simctl uninstall "${DEVICE}" "${APP_ID}"
-set -e
+# DEVICE="iPhone 11 Pro"
+# APP_ID="io.gnosis.multisig.staging.rinkeby"
+# set +e && xcrun simctl boot "${DEVICE}" 2>/dev/null
+# xcrun simctl uninstall "${DEVICE}" "${APP_ID}"
+# set -e
 ############
 
 set -o pipefail && \
