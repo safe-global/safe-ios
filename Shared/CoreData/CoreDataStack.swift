6c10799f49697c8e235848872e5cea3d7e69e0d6
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2021-07-26 13:39:36 +0200

    gh-1344 Migrating store to new location

--- Shared/CoreData/CoreDataStack.swift
+++ Shared/CoreData/CoreDataStack.swift
@@ -13,15 +13,34 @@ import Combine
 class CoreDataStack: CoreDataProtocol {
     private var subscribers = Set<AnyCancellable>()
 
+    @ConfigurationKey("APP_GROUP_ID")
+    static var appGroupId: String
+
+    // Loads core data store and migrates from previous versions if needed.
     lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
+        // By default, container configured with database located in app sandbox.
         let container = NSPersistentContainer(name: "Multisig")
-        container.loadPersistentStores { _, error in
+
+        // Get the default location
+        let defaultStoreUrl = container.persistentStoreDescriptions.first?.url
+        let defaultStoreExists = defaultStoreUrl != nil && FileManager.default.fileExists(atPath: defaultStoreUrl!.path)
+
+        // Get app group container location
+        guard let appGroupContainerUrl = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: Self.appGroupId) else {
+            fatalError("Expected to have App Group set up with id: \(Self.appGroupId)")
+        }
+
+        // Check if already migrated default store to the shared app group location
+        let sharedStoreUrl = appGroupContainerUrl.appendingPathComponent("Multisig").appendingPathExtension("sqlite")
+        var sharedStoreExists = FileManager.default.fileExists(atPath: sharedStoreUrl.path)
+
+        let didNotMigrate = defaultStoreExists && !sharedStoreExists
+
+        if !didNotMigrate {
+            container.persistentStoreDescriptions = [NSPersistentStoreDescription(url: sharedStoreUrl)]
+        }
+
+        container.loadPersistentStores { [unowned container] storeDescription, error in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -35,19 +54,42 @@ class CoreDataStack: CoreDataProtocol {
                  Check the error message to determine what the actual problem was.
                  */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
-            } else {
-                // merge changes from background contexts into the view context
-                NotificationCenter.default
+            }
+
+
+            // migrate store if needed
+            if didNotMigrate,
+               let defaultStore = container.persistentStoreCoordinator.persistentStore(for: defaultStoreUrl!) {
+                do {
+                    try container.persistentStoreCoordinator.migratePersistentStore(
+                        defaultStore,
+                        to: sharedStoreUrl,
+                        options: nil,
+                        withType: defaultStore.type)
+
+                    sharedStoreExists = true
+                } catch {
+                    print("Failed to migrate persistent store to new location: \(error)")
+                    sharedStoreExists = false
+                }
+            }
+
+            if defaultStoreExists && sharedStoreExists {
+                try? FileManager.default.removeItem(at: defaultStoreUrl!)
+            }
+
+            // merge changes from background contexts into the view context
+            NotificationCenter.default
                 .publisher(for: Notification.Name.NSManagedObjectContextDidSave)
                 .receive(on: RunLoop.main)
                 .sink { [unowned container] notification in
-                        let savedMOC = notification.object as! NSManagedObjectContext
-                        guard savedMOC != container.viewContext,
-                            savedMOC.persistentStoreCoordinator == container.persistentStoreCoordinator else { return }
-                        container.viewContext.mergeChanges(fromContextDidSave: notification)
+                    let savedMOC = notification.object as! NSManagedObjectContext
+                    guard savedMOC != container.viewContext,
+                          savedMOC.persistentStoreCoordinator == container.persistentStoreCoordinator else { return }
+                    container.viewContext.mergeChanges(fromContextDidSave: notification)
                 }
                 .store(in: &self.subscribers)
-            }
+
         }
         return container
     }()
