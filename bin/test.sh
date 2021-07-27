09aed1cf5262e8e0f9567328c8ff7a64b20f38b3
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2021-07-27 12:29:12 +0200

    gh-1344 Attempt 2

--- bin/test.sh
+++ bin/test.sh
@@ -14,11 +14,12 @@ bin/configure.sh
 
 ############
 # uncomment when you need to delete the app from the simulator
-# DEVICE="iPhone 11 Pro"
-# APP_ID="io.gnosis.multisig.staging.rinkeby"
-# set +e && xcrun simctl boot "${DEVICE}" 2>/dev/null
-# xcrun simctl uninstall "${DEVICE}" "${APP_ID}"
-# set -e
+# (happens when introducing coredata breaking changes :-( )
+DEVICE="iPhone 11 Pro"
+APP_ID="io.gnosis.multisig.staging.mainnet"
+set +e && xcrun simctl boot "${DEVICE}" 2>/dev/null
+xcrun simctl uninstall "${DEVICE}" "${APP_ID}"
+set -e
 ############
 
 set -o pipefail && \
