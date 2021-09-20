a567f506863812fabed43f068b26453dea57a30c
Andrey Scherbovich <andrey@gnosis.io>
2021-09-20 13:37:51 +0300

    gh-1356 update WalletConnect lib

--- Multisig/UI/Transaction/TransactionDetailsViewController/TransactionDetailsViewController.swift
+++ Multisig/UI/Transaction/TransactionDetailsViewController/TransactionDetailsViewController.swift
@@ -346,8 +346,9 @@ class TransactionDetailsViewController: LoadableViewController, UITableViewDataS
         super.reloadData()
 
         do {
-            let safeAddress = try Address(from: safe.address!)
+            let safeAddress = try Address(from: safe.address!)            
             transaction.safe = AddressString(safeAddress)
+            transaction.chainId = safe.chain!.id
         } catch {
             onError(GSError.error(description: "Failed to execute transaction", error: error))
         }
