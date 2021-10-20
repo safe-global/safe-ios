4e54779227160c0a524ffef5f3ff8c237488fe50
Mouaz <Mouaz.alzahabi@gnosis.pm>
2021-10-20 14:13:08 +0200

    Create Addressbook model

---
+++ Multisig/Logic/Models/AddressBookEntity.swift
@@ -0,0 +1,133 @@
+//
+//  AddressBookEntity.swift
+//  Multisig
+//
+//  Created by Andrey Scherbovich on 20/10/21.
+//  Copyright © 2021 Gnosis Ltd. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+// "address:chainId" -> name
+fileprivate var cachedNames = [String: String]()
+
+extension AddressBookEntity: Identifiable {
+    var displayAddress: String { address! }
+    var addressValue: Address { Address(address!)! }
+
+    static var count: Int {
+        let context = App.shared.coreDataStack.viewContext
+        return (try? context.count(for: AddressBookEntity.fetchRequest().all())) ?? 0
+    }
+
+    static var all: [AddressBookEntity] {
+        let context = App.shared.coreDataStack.viewContext
+        return (try? context.fetch(AddressBookEntity.fetchRequest().all())) ?? []
+    }
+
+    static func by(address: String, chainId: String) -> AddressBookEntity? {
+        dispatchPrecondition(condition: .onQueue(.main))
+        let context = App.shared.coreDataStack.viewContext
+        let fr = AddressBookEntity.fetchRequest().by(address: address, chainId: chainId)
+        return try? context.fetch(fr).first
+    }
+
+    static func updateCachedNames() {
+        guard let entities = try? AddressBookEntity.getAll() else { return }
+
+        cachedNames = entities.reduce(into: [String: String]()) { names, entity in
+            let chainId = entity.chain != nil ? entity.chain!.id! : "1"
+            let key = "\(entity.displayAddress):\(chainId)"
+            names[key] = entity.name!
+        }
+    }
+
+    static func cachedName(by address: AddressString, chainId: String) -> String? {
+        let key = "\(address.description):\(chainId)"
+        return cachedNames[key]
+    }
+
+    public override func awakeFromInsert() {
+        super.awakeFromInsert()
+        additionDate = Date()
+    }
+
+    static func getAll() throws -> [AddressBookEntity] {
+        do {
+            let context = App.shared.coreDataStack.viewContext
+            let fr = AddressBookEntity.fetchRequest().all()
+            let safes = try context.fetch(fr)
+            return safes
+        } catch {
+            throw GSError.DatabaseError(reason: error.localizedDescription)
+        }
+    }
+
+    static func exists(_ address: String, chainId: String) -> Bool {
+        by(address: address, chainId: chainId) != nil
+    }
+
+    @discardableResult
+    static func create(address: String, name: String, chain: Chain) -> AddressBookEntity {
+        dispatchPrecondition(condition: .onQueue(.main))
+        let context = App.shared.coreDataStack.viewContext
+
+        let entity = AddressBookEntity(context: context)
+        entity.address = address
+        entity.name = name
+        entity.chain = chain
+
+        App.shared.coreDataStack.saveContext()
+
+        updateCachedNames()
+
+        return entity
+    }
+
+    func update(name: String) {
+        dispatchPrecondition(condition: .onQueue(.main))
+
+        self.name = name
+
+        App.shared.coreDataStack.saveContext()
+
+
+        AddressBookEntity.updateCachedNames()
+
+        NotificationCenter.default.post(name: .addressbookChanged, object: nil)
+    }
+
+    static func remove(entity: AddressBookEntity) {
+        let context = App.shared.coreDataStack.viewContext
+        context.delete(entity)
+        App.shared.coreDataStack.saveContext()
+        updateCachedNames()
+        NotificationCenter.default.post(name: .addressbookChanged, object: nil)
+    }
+
+    static func removeAll() throws {
+        Chain.removeAll()
+        NotificationCenter.default.post(name: .selectedSafeChanged, object: nil)
+    }
+}
+
+extension AddressBookEntity {
+    func update(address: Address, name: String) {
+        self.name = name
+    }
+}
+
+extension NSFetchRequest where ResultType == AddressBookEntity {
+    func all() -> Self {
+        sortDescriptors = [NSSortDescriptor(keyPath: \AddressBookEntity.additionDate, ascending: true)]
+        return self
+    }
+
+    func by(address: String, chainId: String) -> Self {
+        sortDescriptors = []
+        predicate = NSPredicate(format: "address == %@ AND chain.id == %@", address, chainId)
+        fetchLimit = 1
+        return self
+    }
+}
