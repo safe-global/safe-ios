df03742ec79381b65b26d83060d47836325ab302
Vitaly Katz <vitaly@gnosis.io>
2021-11-25 09:47:05 +0100

    Load TestingAppDelegate instead of AppDelegate for unit tests

--- Multisig/App/AppDelegate.swift
+++ Multisig/App/AppDelegate.swift
@@ -9,7 +9,6 @@
 import UIKit
 import SwiftUI
 
-@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         App.shared.firebaseConfig.setUp()
