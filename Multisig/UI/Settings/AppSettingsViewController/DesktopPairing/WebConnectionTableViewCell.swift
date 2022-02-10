ab52f1e961ab5198a12fbd84ce530d77d30fa269
Vitaly Katz <vitaly@gnosis.io>
2022-02-10 08:11:52 +0100

    gh-1900 Add table view cell for web connection

---
+++ Multisig/UI/Settings/AppSettingsViewController/DesktopPairing/WebConnectionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WebConnectionTableViewCell.swift
+//  Multisig
+//
+//  Created by Vitaly on 09.02.22.
+//  Copyright © 2022 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class WebConnectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
