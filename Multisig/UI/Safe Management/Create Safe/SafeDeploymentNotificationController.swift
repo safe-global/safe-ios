c2a809c310bf47d87aa656380905f88d3d58356c
Dirk Jäckel <github@dirk.jaeckel.name>
2022-03-03 15:42:37 +0100

    GH-2028 Ellipsize name if too long (#2037)
    * GH-2028 Ellipsize name if too long
    * GH-2028 Use ellipsis character and prefix() instead of substr()

--- Multisig/UI/Safe Management/Create Safe/SafeDeploymentNotificationController.swift
+++ Multisig/UI/Safe Management/Create Safe/SafeDeploymentNotificationController.swift
@@ -18,12 +18,13 @@ class SafeDeploymentNotificationController {
         let adressString = "\(shortNamePrefix)\(safeAddress)"
         let chainName = safe.chain!.name ?? ""
         let safeName = safe.name ?? safeAddress
+        let safeNameForTitle = safeName.ellipsize()
 
         let notificationId: String
         let content = UNMutableNotificationContent()
         switch safe.safeStatus {
         case .deployed:
-            content.title = #"Safe "\#(safeName)" created!"#
+            content.title = #"Safe "\#(safeNameForTitle)" created!"#
             content.userInfo["type"] = "safeCreated"
             notificationId = "safeCreated_\(safe.chain!.id!):\(safe.address!)"
 
@@ -39,10 +40,10 @@ class SafeDeploymentNotificationController {
         content.body = "\(adressString) (\(chainName))"
         content.userInfo["safe"] = safe.address!
         content.userInfo["chainId"] = safe.chain!.id!
-        
+
         // no trigger to deliver immediately
         let request = UNNotificationRequest(identifier: notificationId, content: content, trigger: nil)
-        
+
         // Schedule the request with the system.
         let notificationCenter = UNUserNotificationCenter.current()
         notificationCenter.add(request) { (error) in
@@ -57,11 +58,21 @@ class SafeDeploymentNotificationController {
         let known = ["safeCreated", "safeCreationFailed"].contains(type)
         return known
     }
-    
-    static func handleSafeCreatedNotification(userInfo: [AnyHashable : Any]) {
+
+    static func handleSafeCreatedNotification(userInfo: [AnyHashable: Any]) {
         let address: String = userInfo["safe"] as! String
         let chainId: String = userInfo["chainId"] as! String
         let route = NavigationRoute.showAssets(address, chainId: chainId)
         DefaultNavigationRouter.shared.navigate(to: route)
+    }
+}
+
+extension String {
+    func ellipsize(maxLength: Int = 14) -> String {
+        if self.count > maxLength {
+            var shortString = self.prefix(maxLength)
+            return shortString.trimmingCharacters(in: .whitespaces) + "…"
+        }
+        return self
     }
 }
