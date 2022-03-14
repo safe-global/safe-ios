7c0b6d1bdad4ec20692f8be5c9e6873b44831d14
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2022-03-14 15:39:36 +0100

    gh-2052 Implementing data conversion

--- Multisig/Features/Connect to Web/Logic/WebConnectionToSessionTransformer.swift
+++ Multisig/Features/Connect to Web/Logic/WebConnectionToSessionTransformer.swift
@@ -11,49 +11,58 @@ import Ethereum
 class WebConnectionToSessionTransformer {
 
     func update(connection: WebConnection, with session: Session) {
-        // TODO: adapt for the other way around (myself == dapp, other == wallet)
-        // This implementation assumes the local peer is wallet and remote is a dapp.
-        // It is done this way to reduce the scope of work. It is incomplete, but should work for connecting to
-        // the safe web app.
-        assert(connection.localPeer != nil)
-        assert(connection.remotePeer != nil)
-        guard var myself = connection.localPeer, var other = connection.remotePeer else {
+        guard let myself = connection.localPeer, let other = connection.remotePeer else {
+            assertionFailure("Local or remote peer are not set")
             return
         }
-        assert(myself.role == .wallet)
-        assert(other.role == .dapp)
-
-        // if dapp didn't suggest chain id, pick mainnet by default
-        connection.chainId = session.dAppInfo.chainId ?? Int(Chain.ChainID.ethereumMainnet)!
-        update(peer: other, with: session.dAppInfo)
+        if myself.role == .wallet && other.role == .dapp {
+            // if dapp didn't suggest chain id, pick mainnet by default
+            connection.chainId = session.dAppInfo.chainId ?? Int(Chain.ChainID.ethereumMainnet)!
+            other.peerId = session.dAppInfo.peerId
+            update(peer: other, with: session.dAppInfo.peerMeta)
+        } else if myself.role == .dapp && other.role == .wallet, let walletInfo = session.walletInfo {
+            connection.accounts = walletInfo.accounts.compactMap(Address.init)
+            connection.chainId = walletInfo.chainId
+            other.peerId = walletInfo.peerId
+            update(peer: other, with: walletInfo.peerMeta)
+        }
     }
 
-    private func update(peer: WebConnectionPeerInfo, with other: Session.DAppInfo) {
-        peer.peerId = other.peerId
-        peer.url = other.peerMeta.url
-        peer.name = other.peerMeta.name
-        peer.description = other.peerMeta.description
-        peer.icons = other.peerMeta.icons
-        peer.deeplinkScheme = other.peerMeta.scheme
+    private func update(peer: WebConnectionPeerInfo, with peerMeta: Session.ClientMeta) {
+        peer.url = peerMeta.url
+        peer.name = peerMeta.name
+        peer.description = peerMeta.description
+        peer.icons = peerMeta.icons
+        peer.deeplinkScheme = peerMeta.scheme
     }
 
     func session(from connection: WebConnection) -> Session? {
-        guard let localPeer = connection.localPeer,
+        guard let myself = connection.localPeer,
               let remotePeer = connection.remotePeer
         else {
             return nil
         }
-        assert(localPeer.role == .wallet)
-        assert(remotePeer.role == .dapp)
-
-        let dappInfo: Session.DAppInfo = dappInfo(from: remotePeer)
-        let walletInfo: Session.WalletInfo = walletInfo(from: localPeer, connection: connection)
-        let result = Session(
-            url: connection.connectionURL.wcURL,
-            dAppInfo: dappInfo,
-            walletInfo: walletInfo
-        )
-        return result
+        if myself.role == .wallet && remotePeer.role == .dapp {
+            let dappInfo: Session.DAppInfo = dappInfo(from: remotePeer)
+            let walletInfo: Session.WalletInfo = walletInfo(from: myself, connection: connection)
+            let result = Session(
+                url: connection.connectionURL.wcURL,
+                dAppInfo: dappInfo,
+                walletInfo: walletInfo
+            )
+            return result
+        } else if myself.role == .dapp && remotePeer.role == .wallet {
+            let dappInfo = self.dappInfo(from: myself)
+            let walletInfo = self.walletInfo(from: remotePeer, connection: connection)
+            let result = Session(
+                url: connection.connectionURL.wcURL,
+                dAppInfo: dappInfo,
+                walletInfo: walletInfo
+            )
+            return result
+        } else {
+            return nil
+        }
     }
 
     private func walletInfo(from peer: WebConnectionPeerInfo, connection: WebConnection) -> Session.WalletInfo {
