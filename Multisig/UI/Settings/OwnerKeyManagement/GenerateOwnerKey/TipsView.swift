06a09dab74d50b8cfb1061516b5ede8d76ecf8e9
Vitaly Katz <vitaly@gnosis.io>
2022-04-12 09:18:56 +0200

    gh-2180 Custom view for tips list

---
+++ Multisig/UI/Settings/OwnerKeyManagement/GenerateOwnerKey/TipsView.swift
@@ -0,0 +1,51 @@
+//
+//  TipsView.swift
+//  Multisig
+//
+//  Created by Vitaly on 11.04.22.
+//  Copyright © 2022 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class TipsView: UINibView {
+
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tipsContainer: UIStackView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        titleLabel.setStyle(.primary)
+        backgroundView.backgroundColor = .backgroundPrimary
+        backgroundView.layer.cornerRadius = 8
+    }
+    
+    func setContent(title: String, tips: [String]) {
+        
+        titleLabel.text = title
+        tipsContainer.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        for tip in tips {
+            
+            let row = UIStackView()
+            row.axis = .horizontal
+            row.alignment = .firstBaseline
+            row.distribution = .fill
+            row.spacing = 8
+                    
+            let bulletPointLabel = UILabel()
+            bulletPointLabel.setStyle(.secondary)
+            bulletPointLabel.text = "\u{2022}"
+
+            let tipLabel = UILabel()
+            tipLabel.setStyle(.secondary)
+            tipLabel.numberOfLines = 0
+            tipLabel.text = tip
+            
+            row.addArrangedSubview(bulletPointLabel)
+            row.addArrangedSubview(tipLabel)
+            
+            tipsContainer.addArrangedSubview(row)
+        }
+    }
+}
