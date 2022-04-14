d1d7b2df4bb181dd51dd3b6381d4fe59cd07fde6
Vitaly Katz <vitaly@gnosis.io>
2022-04-14 13:24:33 +0200

    gh-2182 Embed SeedPhraseViewController into backup seed phrase screen

--- Multisig/UI/Settings/OwnerKeyManagement/Backup/BackupSeedPhraseViewController.swift
+++ Multisig/UI/Settings/OwnerKeyManagement/Backup/BackupSeedPhraseViewController.swift
@@ -8,14 +8,13 @@
 
 import UIKit
 
-//FIXME: extract base functionality from BackupSeedPhraseViewController and SeedPhraseViewController to a base class
-class BackupSeedPhraseViewController: UIViewController {
-
-    @IBOutlet weak var infoLabel: UILabel!
-    @IBOutlet weak var seedPhraseView: SeedPhraseView!
-    @IBOutlet weak var warningView: WarningView!
-    @IBOutlet weak var copyToClipboardButton: UIButton!
+ 
+class BackupSeedPhraseViewController: ContainerViewController {
+    
+    @IBOutlet weak var seedPhraseContentView: UIView!
     @IBOutlet weak var continueButton: UIButton!
+    
+    var seedPhraseViewController: SeedPhraseViewController!
     
     var seedPhrase: [String] = []
     
@@ -24,17 +23,16 @@ class BackupSeedPhraseViewController: UIViewController {
         
         navigationItem.title = "Back up manually"
         
-        infoLabel.text = "Make sure to store your seed phrase in a secure place. You will need to verify it in the next step."
-        infoLabel.setStyle(.secondary)
-
-        warningView.set(description: "Gnosis Safe will never ask for your seed phrase! It is encrypted and stored locally on your device.")
-
-        copyToClipboardButton.setText("Copy to Clipboard", .primary)
-
-        seedPhraseView.words = seedPhrase.enumerated().map {
-            SeedWord(index: $0.offset, value: $0.element)
-        }
+        seedPhraseViewController = SeedPhraseViewController()
+        seedPhraseViewController.seedPhrase = seedPhrase
+        //TODO: adjust tracking event
+        seedPhraseViewController.trackingEvent = .exportSeed
+        seedPhraseViewController.infoText = "Make sure to store your seed phrase in a secure place. You will need to verify it in the next step."
         
+        viewControllers = [seedPhraseViewController]
+        
+        displayChild(at: 0, in: seedPhraseContentView)
+       
         continueButton.setText("Continue", .filled)
         
         NotificationCenter.default.addObserver(self, selector: #selector(screenshotTaken), name: UIApplication.userDidTakeScreenshotNotification, object: nil)
@@ -45,25 +43,5 @@ class BackupSeedPhraseViewController: UIViewController {
     }
     
     @IBAction func didTapContinueButton(_ sender: Any) {
-    }
-    
-    @IBAction func didTapCopyButton(_ sender: Any) {
-        export(seedPhrase.joined(separator: " "))
-    }
-    
-    func export(_ value: String) {
-        let vc = UIActivityViewController(activityItems: [value], applicationActivities: nil)
-        present(vc, animated: true, completion: nil)
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        seedPhraseView.update()
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        Tracker.trackEvent(.exportSeed)
-        seedPhraseView.update()
     }
 }
