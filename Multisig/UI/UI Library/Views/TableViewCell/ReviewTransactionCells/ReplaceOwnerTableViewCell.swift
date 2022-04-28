c288f987c6459468c85f98c1b1d2d10562795ae0
Vitaly Katz <vitaly@gnosis.io>
2022-04-28 14:31:53 +0200

    gh-2258 Add replace icon

---
+++ Multisig/UI/UI Library/Views/TableViewCell/ReviewTransactionCells/ReplaceOwnerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReplaceOwnerTableViewCell.swift
+//  Multisig
+//
+//  Created by Vitaly on 28.04.22.
+//  Copyright © 2022 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ReplaceOwnerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
