1b2afea2f0ed6d8725b77604b060420a0c8ff4b2
Vitaly Katz <vitaly@gnosis.io>
2022-05-13 17:49:26 +0200

    gh-2275 Refactor to remove code duplication

--- Multisig/UI/Settings/OwnerKeyManagement/AddKeyAsNewOwner/AddOwnerFlow.swift
+++ Multisig/UI/Settings/OwnerKeyManagement/AddKeyAsNewOwner/AddOwnerFlow.swift
@@ -65,28 +65,66 @@ class AddOwnerFlow: UIFlow {
 }
 
 class AddOwnerFlowFactory {
+
     func confirmations(safe: Safe, completion: @escaping (_ newConfirmations: Int) -> Void) -> EditConfirmationsViewController {
+        return confirmations(
+            safe: safe,
+            minConfirmations: 1,
+            maxConfirmations: max(1, (safe.ownersInfo ?? []).count) + 1,
+            stepNumber: 1,
+            maxSteps: 2,
+            trackingEvent: .addAsOwnerChangeConfirmations,
+            completion: completion)
+    }
+
+    fileprivate func confirmations(
+        safe: Safe,
+        minConfirmations: Int,
+        maxConfirmations: Int,
+        stepNumber: Int,
+        maxSteps: Int,
+        trackingEvent: TrackingEvent,
+        completion: @escaping (_ newConfirmations: Int) -> Void
+    ) -> EditConfirmationsViewController {
         let confirmationsVC = EditConfirmationsViewController()
-        confirmationsVC.confirmations = Int(safe.threshold ?? 0)
-        confirmationsVC.minConfirmations = 1
-        confirmationsVC.maxConfirmations = max(1, (safe.ownersInfo ?? []).count) + 1
-        confirmationsVC.stepNumber = 1
-        confirmationsVC.maxSteps = 2
-        confirmationsVC.trackingEvent = .addAsOwnerChangeConfirmations
+        confirmationsVC.confirmations = Int(safe.threshold ?? 1)
+        confirmationsVC.minConfirmations = minConfirmations
+        confirmationsVC.maxConfirmations = maxConfirmations
+        confirmationsVC.stepNumber = stepNumber
+        confirmationsVC.maxSteps = maxSteps
+        confirmationsVC.trackingEvent = trackingEvent
         confirmationsVC.completion = completion
         confirmationsVC.showPromptLabel = true
         return confirmationsVC
     }
 
     func review(safe: Safe, key: AddressInfo, newThreshold: Int, completion: @escaping (SCGModels.TransactionDetails) -> Void) -> ReviewAddOwnerTxViewController {
+        return review(
+            safe: safe,
+            key: key,
+            newThreshold: newThreshold,
+            stepNumber: 2,
+            maxSteps: 2,
+            completion: completion
+        )
+    }
+
+    fileprivate func review(
+        safe: Safe,
+        key: AddressInfo,
+        newThreshold: Int,
+        stepNumber: Int,
+        maxSteps: Int,
+        completion: @escaping (SCGModels.TransactionDetails) -> Void
+    ) -> ReviewAddOwnerTxViewController {
         let addOwnerReviewVC = ReviewAddOwnerTxViewController(
             safe: safe,
             owner: key,
             oldOwnersCount: safe.ownersInfo?.count ?? 0,
             oldThreshold: Int(safe.threshold ?? 0),
             newThreshold: newThreshold)
-        addOwnerReviewVC.stepNumber = 2
-        addOwnerReviewVC.maxSteps = 2
+        addOwnerReviewVC.stepNumber = stepNumber
+        addOwnerReviewVC.maxSteps = maxSteps
         addOwnerReviewVC.onSuccess = completion
         return addOwnerReviewVC
     }
@@ -106,39 +144,24 @@ class AddOwnerFlowFactory {
 class AddOwnerFlowFromSettingsFactory: AddOwnerFlowFactory {
 
     override func confirmations(safe: Safe, completion: @escaping (_ newConfirmations: Int) -> Void) -> EditConfirmationsViewController {
-        let confirmationsVC = EditConfirmationsViewController()
-        confirmationsVC.confirmations = Int(safe.threshold ?? 1)
-        confirmationsVC.minConfirmations = 1
-        confirmationsVC.maxConfirmations = max(1, (safe.ownersInfo ?? []).count) + 1
-        confirmationsVC.stepNumber = 2
-        confirmationsVC.maxSteps = 3
-        confirmationsVC.trackingEvent = .addAsOwnerChangeConfirmations
-        confirmationsVC.completion = completion
-        confirmationsVC.showPromptLabel = true
-        return confirmationsVC
+        return confirmations(
+            safe: safe,
+            minConfirmations: 1,
+            maxConfirmations: max(1, (safe.ownersInfo ?? []).count) + 1,
+            stepNumber: 2,
+            maxSteps: 3,
+            trackingEvent: .addAsOwnerChangeConfirmations,
+            completion: completion)
     }
 
     override func review(safe: Safe, key: AddressInfo, newThreshold: Int, completion: @escaping (SCGModels.TransactionDetails) -> Void) -> ReviewAddOwnerTxViewController {
-        let addOwnerReviewVC = ReviewAddOwnerTxViewController(
+        return review(
             safe: safe,
-            owner: key,
-            oldOwnersCount: safe.ownersInfo?.count ?? 0,
-            oldThreshold: Int(safe.threshold ?? 0),
-            newThreshold: newThreshold)
-        addOwnerReviewVC.stepNumber = 3
-        addOwnerReviewVC.maxSteps = 3
-        addOwnerReviewVC.onSuccess = completion
-        return addOwnerReviewVC
-    }
-
-    override func success(completion: @escaping (_ showTxDetails: Bool) -> Void) -> SuccessViewController {
-        let successVC = SuccessViewController(
-            titleText: "Your transaction is submitted!",
-            bodyText: "It needs to be confirmed and executed first before the owner will be added.",
-            primaryAction: "View transaction details",
-            secondaryAction: "Done",
-            trackingEvent: .addAsOwnerSuccess)
-        successVC.onDone = completion
-        return successVC
+            key: key,
+            newThreshold: newThreshold,
+            stepNumber: 3,
+            maxSteps: 3,
+            completion: completion
+        )
     }
 }
