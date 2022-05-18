1273247db2a1f1cc0ffcaad8a1d19f725c61badc
Dirk Jäckel <github@dirk.jaeckel.name>
2022-05-18 15:02:20 +0200

    GH-2335 Log file/line/function name of the place where the tracking was triggered.

--- Multisig/Cross-layer/Analytics/ConsoleTracker.swift
+++ Multisig/Cross-layer/Analytics/ConsoleTracker.swift
@@ -9,15 +9,15 @@ final class ConsoleTracker: TrackingHandler {
     private var trackingEnabled = true
 
     /// You will find the events by the "[TRACKING]" tag in the log.
-    func track(event: String, parameters: [String: Any]?) {
+    func track(event: String, parameters: [String: Any]?, file: StaticString = #file, line: UInt = #line, function: StaticString = #function) {
         guard trackingEnabled else { return }
         let parametersString = parameters != nil ? (", parameters: " + String(describing: parameters!)) : ""
-        LogService.shared.info("[TRACKING] event: '\(event)'" + parametersString)
+        LogService.shared.info("[TRACKING] event: '\(event)'" + parametersString, file: file, line: line, function: function)
     }
 
-    func setUserProperty(_ value: String, for property: UserProperty) {
+    func setUserProperty(_ value: String, for property: UserProperty, file: StaticString = #file, line: UInt = #line, function: StaticString = #function) {
         guard trackingEnabled else { return }
-        LogService.shared.info("[TRACKING] setUserProperty: '\(value)' for \(property.rawValue)")
+        LogService.shared.info("[TRACKING] setUserProperty: '\(value)' for \(property.rawValue)", file: file, line: line, function: function)
     }
 
     func setTrackingEnabled(_ value: Bool) {
