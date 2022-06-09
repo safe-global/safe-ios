986ffb04dadda79740ed423c3a0b87c0b055dd51
Dirk Jäckel <github@dirk.jaeckel.name>
2022-06-09 18:35:35 +0200

    GH-1842 More tests

--- MultisigTests/Logic/Models/Transaction/DelegateWarningCalculatorTests.swift
+++ MultisigTests/Logic/Models/Transaction/DelegateWarningCalculatorTests.swift
@@ -38,4 +38,31 @@ class DelegateWarningCalculatorTests: XCTestCase {
 
         XCTAssertTrue(result)
     }
+
+    func testMultiSendInnerWarningAndNotTopLevel() throws {
+        // https://safe-client.gnosis.io/v1/chains/4/transactions/multisig_0x73a7AA145338587f7aB7f63c06d187C85dF4727e_0xba28109747222d6cfb7f0fb75f03d49957e343674bc116162b038e244dbcc6d6
+        let data = jsonData("DelegateWarningNotTopLevel")
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .millisecondsSince1970
+        let decodedData = try decoder.decode(SCGModels.TransactionDetails.self, from: data)
+        let txData: SCGModels.TxData = decodedData.txData!
+
+        let result = DelegateWarningCalculator.isUntrusted(txData: txData)
+
+        XCTAssertTrue(result)
+    }
+
+    func testMultiSendOperationIsDelegateButToAddressIsKnown() throws {
+        // https://safe-client.gnosis.io/v1/chains/4/transactions/multisig_0x73a7AA145338587f7aB7f63c06d187C85dF4727e_0xba28109747222d6cfb7f0fb75f03d49957e343674bc116162b038e244dbcc6d6
+        let data = jsonData("NoDelegateWarningBecauseMultiSendAddressIsKnown")
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .millisecondsSince1970
+        let decodedData = try decoder.decode(SCGModels.TransactionDetails.self, from: data)
+        let txData: SCGModels.TxData = decodedData.txData!
+
+        let result = DelegateWarningCalculator.isUntrusted(txData: txData)
+
+        // Multisend operation is delegate but to address is known
+        XCTAssertFalse(result)
+    }
 }
