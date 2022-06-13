8cc419be926f4eee8ca1c11d2b669429298bdd31
Mouaz <Mouaz.alzahabi@gnosis.pm>
2022-06-13 16:19:46 +0200

    Add screen

---
+++ Multisig/UI/Settings/OwnerKeyManagement/GenerateOwnerKey/ShareAddOwnerLinkViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ShareAddOwnerLinkViewController.swift
+//  Multisig
+//
+//  Created by Moaaz on 6/12/22.
+//  Copyright © 2022 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ShareAddOwnerLinkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
