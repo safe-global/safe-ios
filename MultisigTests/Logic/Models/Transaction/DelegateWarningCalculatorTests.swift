5c2953754753cb39b59839cacb8e58e2bbef3187
Dirk Jäckel <github@dirk.jaeckel.name>
2022-06-13 19:02:47 +0200

    GH-1842 More tests

--- MultisigTests/Logic/Models/Transaction/DelegateWarningCalculatorTests.swift
+++ MultisigTests/Logic/Models/Transaction/DelegateWarningCalculatorTests.swift
@@ -110,6 +110,24 @@ class DelegateWarningCalculatorTests: XCTestCase {
         XCTAssertFalse(result)
     }
 
+    func testTransferNoWarningButDelegate() throws {
+        let txData = try loadAndParseFile(fileName: "TransferNoWarningButDelegate")
+
+        let result = DelegateWarningCalculator.isUntrusted(txData: txData)
+
+        // Delegate flag not set. But Delegate with unknown Address -> Warning
+        XCTAssertTrue(result)
+    }
+
+    func testTransferNoWarningButDelegateWithName() throws {
+        let txData = try loadAndParseFile(fileName: "TransferNoWarningButDelegateWithName")
+
+        let result = DelegateWarningCalculator.isUntrusted(txData: txData)
+
+        // Delegate flag not set. But Delegate with known Address -> No Warning
+        XCTAssertFalse(result)
+    }
+
     // Helper
     private func loadAndParseFile(fileName: String) throws -> TxData {
         let data = jsonData(fileName)
