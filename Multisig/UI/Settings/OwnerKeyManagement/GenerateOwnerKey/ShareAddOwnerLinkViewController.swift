2b5065c160950c621008eb2aaab2c094353f4602
Mouaz <Mouaz.alzahabi@gnosis.pm>
2022-06-14 12:58:00 +0200

    Add screen

--- Multisig/UI/Settings/OwnerKeyManagement/GenerateOwnerKey/ShareAddOwnerLinkViewController.swift
+++ Multisig/UI/Settings/OwnerKeyManagement/GenerateOwnerKey/ShareAddOwnerLinkViewController.swift
@@ -9,11 +9,46 @@
 import UIKit
 
 class ShareAddOwnerLinkViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet private weak var shareLinkView: ShareTextView!
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var doneButton: UIButton!
+
+    var steps: [Step] = []
+    var onFinish: (() -> ())?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleLabel.setStyle(.title5)
+        doneButton.setText("Done", .filled)
 
-        // Do any additional setup after loading the view.
+        tableView.registerCell(InstructionHeaderTableViewCell.self)
+        tableView.registerCell(FinalStepInstructionTableViewCell.self)
+        tableView.registerCell(StepInstructionTableViewCell.self)
+
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 100
     }
 
+    @IBAction func doneButtonTouched(_ sender: Any) {
+        onFinish?()
+    }
+}
+
+extension ShareAddOwnerLinkViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let currentStep = steps[indexPath.row]
+        let cell = tableView.dequeueCell(StepInstructionTableViewCell.self, for: indexPath)
+        cell.selectionStyle = .none
+        cell.separatorInset.left = .greatestFiniteMagnitude
+        cell.circleLabel.text = number
+        cell.headerLabel.text = title
+        cell.descriptionLabel.text = description
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        steps.count
+    }
 }
---
+++ Multisig/UI/UI Library/Views/ShareTextView.swift
@@ -0,0 +1,21 @@
+//
+//  ShareTextView.swift
+//  Multisig
+//
+//  Created by Moaaz on 6/13/22.
+//  Copyright © 2022 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ShareTextView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
