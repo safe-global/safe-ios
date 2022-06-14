481970802592e5b0ad6eba3be557c9462327b0cd
Vitaly Katz <vitaly@gnosis.io>
2022-06-14 16:42:14 +0200

    gh-2345 Custom view for key blockie with name & address

---
+++ Multisig/UI/Settings/OwnerKeyManagement/MakeKeyAnOwner/KeyInfoView.swift
@@ -0,0 +1,36 @@
+//
+//  KeyInfoView.swift
+//  Multisig
+//
+//  Created by Vitaly on 13.06.22.
+//  Copyright © 2022 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class KeyInfoView: UINibView {
+
+    @IBOutlet weak var blockie: IdenticonView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+
+    override func commonInit() {
+        super.commonInit()
+
+        nameLabel.setStyle(.primary)
+        addressLabel.setStyle(.tertiary)
+    }
+
+    func set(owner: AddressInfo, badgeName: String? = nil, reqConfirmations: Int? = nil, ownerCount: Int? = nil) {
+
+        blockie.set(address: owner.address, badgeName: badgeName, reqConfirmations: reqConfirmations, owners: ownerCount)
+
+        if let name = owner.name {
+            nameLabel.text = name
+        } else {
+            nameLabel.isHidden = true
+        }
+        addressLabel.text = owner.address.ellipsized()
+    }
+}
+
