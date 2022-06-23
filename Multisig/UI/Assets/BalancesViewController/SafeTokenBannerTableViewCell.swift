7862b399b414b5662a20b4539324377f970ee10b
Vitaly Katz <vitaly@gnosis.io>
2022-06-23 15:28:29 +0200

    gh-2432 Add tracking events

---
+++ Multisig/UI/Assets/BalancesViewController/SafeTokenBannerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SafeTokenBannerTableViewCell.swift
+//  Multisig
+//
+//  Created by Vitaly on 23.06.22.
+//  Copyright © 2022 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SafeTokenBannerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
