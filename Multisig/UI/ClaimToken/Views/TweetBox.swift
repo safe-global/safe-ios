594e36251ebcf01cdfcc3e7d68f119fe8722fe17
Vitaly Katz <vitaly@gnosis.io>
2022-08-02 08:29:42 +0200

    gh-2503 Add custom tweet box view

---
+++ Multisig/UI/ClaimToken/Views/TweetBox.swift
@@ -0,0 +1,48 @@
+//
+//  TweetBox.swift
+//  Multisig
+//
+//  Created by Vitaly on 02.08.22.
+//  Copyright © 2022 Gnosis Ltd. All rights reserved.
+//
+
+
+import UIKit
+
+class TweetBox: UINibView {
+    
+    @IBOutlet weak var tweetLabel: UILabel!
+    @IBOutlet weak var tweetButton: UIButton!
+
+    override func commonInit() {
+        super.commonInit()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        layer.borderWidth = 10
+        layer.borderColor = UIColor.backgroundPrimary.cgColor
+        layer.cornerRadius = 8
+
+        tweetLabel.setStyle(.secondary.color(.labelPrimary))
+    }
+
+    func setTweet(text: String, hashtags: [String]) {
+
+        let hashtagsString = hashtags
+            .map {
+                "#\($0)"
+            }
+            .joined(separator: " ")
+
+        tweetLabel.attributedText = text.highlightRange(
+            originalStyle: .secondary.color(.labelPrimary),
+            highlightStyle: .primary.color(.primary),
+            textToHightlight: hashtagsString
+        )
+    }
+
+    @IBAction func didTapTweetButton(_ sender: Any) {
+    }
+}
