bcc9687362195610bf0f6697cf5f754b47b6ac91
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2022-09-01 18:00:41 +0200

    GH-2551 Re-created top cards UI

---
+++ Multisig/UI/ClaimToken/ClaimingAmountViewController/AllocationBoxCell.swift
@@ -0,0 +1,227 @@
+//
+//  AllocationBoxCell.swift
+//  Multisig
+//
+//  Created by Dmitry Bespalov on 01.09.22.
+//  Copyright © 2022 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class AllocationBoxCell: UITableViewCell {
+    @IBOutlet private weak var boxView: UIView!
+    @IBOutlet private weak var contentStack: UIStackView!
+
+    @IBOutlet private weak var headerStack: UIStackView!
+    @IBOutlet private weak var headerLabel: UILabel!
+    @IBOutlet private weak var headerButton: InfoButton!
+
+    @IBOutlet private weak var bodyStack: UIStackView!
+    @IBOutlet private weak var titleStack: UIStackView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var titleButton: InfoButton!
+
+    @IBOutlet private weak var valueLabel: UILabel!
+
+    @IBOutlet private weak var boxOuterTrailing: NSLayoutConstraint!
+    @IBOutlet private weak var boxOuterLeading: NSLayoutConstraint!
+    @IBOutlet private weak var boxOuterTop: NSLayoutConstraint!
+    @IBOutlet private weak var boxOuterBottom: NSLayoutConstraint!
+
+    @IBOutlet private weak var boxInnerTop: NSLayoutConstraint!
+    @IBOutlet private weak var boxInnerBottom: NSLayoutConstraint!
+    @IBOutlet private weak var boxInnerTrailing: NSLayoutConstraint!
+    @IBOutlet private weak var boxInnerLeading: NSLayoutConstraint!
+
+    enum Style {
+        case darkGuardian
+        case darkUser
+        case lightGuardian
+        case lightUser
+    }
+
+    var style: Style = .darkGuardian {
+        didSet {
+            updateStyle()
+        }
+    }
+
+    var boxOuterSpacing: UIEdgeInsets = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
+    var boxInnerSpacing: UIEdgeInsets = UIEdgeInsets(top: 24, left: 24, bottom: 24, right: 24)
+
+    var contentVerticalSpacing: CGFloat = 16
+    var buttonHorizontalSpacing: CGFloat = 8
+    var bodyVerticalSpacing: CGFloat = 0
+
+    var boxBackgroundColor: UIColor = .primary
+
+    var headerStyle: GNOTextStyle = .headline.color(.white)
+    var titleStyle: GNOTextStyle = .Updated.border
+    var valueStyle: GNOTextStyle = .Updated.whiteTitle
+
+    enum TitleImage: String {
+        case dark = "ico-shield-dark"
+        case darkCircle = "ico-shield-circle-dark"
+        case light = "ico-shield-light"
+        case lightCircle = "ico-shield-circle-light"
+    }
+    var headerButtonImageName: String? = "ico-info-24"
+    var headerButtonHidden: Bool = false
+
+    var titleButtonImage: TitleImage? = .darkCircle
+    var titleButtonImageName: String?
+
+    var headerText: String?
+    var titleText: String?
+    var valueText: String?
+
+    var headerTooltipText: NSAttributedString?
+    var titleTooltipText: NSAttributedString?
+
+    weak var tooltipHostView: UIView?
+
+    private var headerTooltip: Tooltip?
+    private var titleTooltip: Tooltip?
+
+    // hide all tooltips before showing
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        style = .darkGuardian
+    }
+
+    @IBAction func didTapAllocationHeaderButton(_ sender: Any) {
+        showTooltip(text: headerTooltipText, existing: &headerTooltip, button: headerButton)
+    }
+
+    @IBAction func didTapAllocationTitleButton(_ sender: Any) {
+        showTooltip(text: titleTooltipText, existing: &titleTooltip, button: titleButton)
+    }
+
+    private func showTooltip(text: NSAttributedString?, existing: inout Tooltip?, button: UIButton) {
+        guard let host = tooltipHostView, let text = text else { return }
+
+        if let tooltip = existing, tooltip.isVisible {
+            tooltip.hide()
+            return
+        }
+
+        NotificationCenter.default.post(name: .hideAllTooltips, object: self)
+
+        existing = Tooltip.show(
+            for: button,
+            in: host,
+            message: nil,
+            attributedText: text,
+            arrowTarget: button,
+            aboveTarget: false,
+            hideAutomatically: true,
+            delegate: nil
+        )
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        headerText = nil
+        titleText = nil
+        valueText = nil
+        headerTooltipText = nil
+        titleTooltipText = nil
+        update()
+    }
+
+    func update() {
+        boxOuterTop.constant = boxOuterSpacing.top
+        boxOuterBottom.constant = boxOuterSpacing.bottom
+        boxOuterLeading.constant = boxOuterSpacing.left
+        boxOuterTrailing.constant = boxOuterSpacing.right
+
+        boxInnerTop.constant = boxInnerSpacing.top
+        boxInnerBottom.constant = boxInnerSpacing.bottom
+        boxInnerLeading.constant = boxInnerSpacing.left
+        boxInnerTrailing.constant = boxInnerSpacing.right
+
+        setNeedsUpdateConstraints()
+
+        contentStack.spacing = contentVerticalSpacing
+        headerStack.spacing = buttonHorizontalSpacing
+        titleStack.spacing = buttonHorizontalSpacing
+        bodyStack.spacing = bodyVerticalSpacing
+
+        boxView.backgroundColor = boxBackgroundColor
+
+        headerLabel.setStyle(headerStyle)
+        headerLabel.text = headerText
+
+        let headerImage = headerButtonImageName.flatMap { UIImage(named: $0) }
+        headerButton.setImage(headerImage, for: .normal)
+        headerButton.isHidden = headerButtonHidden
+
+        titleLabel.setStyle(titleStyle)
+        titleLabel.text = titleText
+
+        let titleImage = (titleButtonImageName ?? titleButtonImage?.rawValue).flatMap { UIImage(named: $0) }
+        titleButton.setImage(titleImage, for: .normal)
+
+        valueLabel.setStyle(valueStyle)
+        valueLabel.text = valueText
+    }
+
+    private func updateStyle() {
+        switch style {
+        case .darkGuardian:
+            boxBackgroundColor = .primary
+            headerStyle = .headline.color(.white)
+            titleStyle = .Updated.border
+            valueStyle = .Updated.whiteTitle
+            headerButtonHidden = true
+            titleButtonImage = .darkCircle
+
+        case .lightGuardian:
+            boxBackgroundColor = .backgroundPrimary
+            headerStyle = .headline
+            titleStyle = .primary.color(.labelSecondary)
+            valueStyle = .Updated.whiteTitle.color(.labelPrimary)
+            headerButtonHidden = false
+            titleButtonImage = .lightCircle
+
+        case .darkUser:
+            boxBackgroundColor = .primary
+            headerStyle = .headline.color(.white)
+            titleStyle = .Updated.border
+            valueStyle = .Updated.whiteTitle
+            headerButtonHidden = true
+            titleButtonImage = .dark
+
+        case .lightUser:
+            boxBackgroundColor = .backgroundPrimary
+            headerStyle = .headline
+            titleStyle = .primary.color(.labelSecondary)
+            valueStyle = .Updated.whiteTitle.color(.labelPrimary)
+            headerButtonHidden = false
+            titleButtonImage = .light
+        }
+
+        update()
+    }
+}
+
+class InfoButton: UIButton {
+    var tapInsets: UIEdgeInsets = UIEdgeInsets(top: -20, left: -20, bottom: -20, right: -20)
+
+    private var tapArea: CGRect {
+        bounds.inset(by: tapInsets)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        tapArea.contains(point)
+    }
+}
--- Multisig/UI/ClaimToken/ClaimingAmountViewController/ClaimingAmountViewController.swift
+++ Multisig/UI/ClaimToken/ClaimingAmountViewController/ClaimingAmountViewController.swift
@@ -11,7 +11,9 @@ import SwiftCryptoTokenFormatter
 
 class ClaimingAmountViewController: LoadableViewController {
     enum RowItem {
-        case claimable
+        case claimableNow
+        case claimableFuture
+        case claimableTotal
         case claimingAmount
     }
 
@@ -24,7 +26,7 @@ class ClaimingAmountViewController: LoadableViewController {
 
     private var stepLabel: UILabel!
 
-    var rows: [RowItem] = [.claimable, .claimingAmount]
+    var rows: [RowItem] = [.claimableNow, .claimableFuture, .claimableTotal, .claimingAmount]
     private let tokenFormatter = TokenFormatter()
 
     convenience init(stepNumber: Int = 3,
@@ -47,10 +49,16 @@ class ClaimingAmountViewController: LoadableViewController {
 
         tableView.registerCell(AvailableClaimingAmountTableViewCell.self)
         tableView.registerCell(EnterClaimingAmountTableViewCell.self)
+        tableView.registerCell(AllocationTotalCell.self)
+
+        tableView.registerCell(AllocationBoxCell.self)
+
         tableView.delegate = self
         tableView.dataSource = self
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 200
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: .greatestFiniteMagnitude, bottom: 0, right: 0)
+
         stepLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 21))
         stepLabel.textAlignment = .right
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: stepLabel)
@@ -76,16 +84,33 @@ extension ClaimingAmountViewController: UITableViewDelegate, UITableViewDataSour
         let row = rows[indexPath.row]
 
         switch row {
-        case .claimable:
-            let cell = tableView.dequeueCell(AvailableClaimingAmountTableViewCell.self)
-
-            cell.set(claimableNowUserAirdropValue: tokenFormatter.string(from: claimingAmount.userAmount.now),
-                     claimableNowEcosystemAirdropValue: tokenFormatter.string(from: claimingAmount.ecosystemAmount.now),
-                     claimableNowTotal: tokenFormatter.string(from: claimingAmount.totalClaimable),
-                     claimableInFutureUserAirdropValue: tokenFormatter.string(from: claimingAmount.userAmount.future),
-                     claimableInFutureEcosystemAirdropValue: tokenFormatter.string(from: claimingAmount.ecosystemAmount.future),
-                     claimableInFutureTotal: tokenFormatter.string(from: claimingAmount.totalClaimableInFuture))
+        case .claimableNow:
+            let cell = tableView.dequeueCell(AllocationBoxCell.self)
+            cell.headerText = "Claim now"
+            cell.titleText = "Total"
+            cell.valueText = "3,000.05 SAFE"
+            cell.titleTooltipText = NSAttributedString(string: "This includes 1000 SAFE for user allocation and 2000.05 SAFE for guardian allocation.")
+            cell.tooltipHostView = view
+            // must be set at last
+            cell.style = .darkGuardian
             return cell
+
+        case .claimableFuture:
+            let cell = tableView.dequeueCell(AllocationBoxCell.self)
+            cell.headerText = "Claim in the future (vesting)"
+            cell.titleText = "Total"
+            cell.valueText = "6,000.10 SAFE"
+            cell.headerTooltipText = NSAttributedString(string: "SAFE vesting is vested linearly over 4 years starting on 01.10.2022, 14:30:00 (Europe/Berlin).")
+            cell.titleTooltipText = NSAttributedString(string: "This includes a Safe guardian allocation of 2000 SAFE.")
+            cell.tooltipHostView = view
+            cell.style = .lightGuardian
+            return cell
+
+        case .claimableTotal:
+            let cell = tableView.dequeueCell(AllocationTotalCell.self)
+            cell.text = "Awarded total allocation is 9000.15 SAFE"
+            return cell
+
         case .claimingAmount:
             let cell = tableView.dequeueCell(EnterClaimingAmountTableViewCell.self)
             cell.set(value: "0",
