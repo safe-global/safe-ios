c4b3771788388a60a0da23e9bcd772739888b5dd
Dirk Jäckel <dirk@novoda.com>
2022-09-05 18:09:20 +0200

    GH-2588 cleanup

--- Multisig/UI/ClaimToken/Tutorial/WhatIsSafeViewController.swift
+++ Multisig/UI/ClaimToken/Tutorial/WhatIsSafeViewController.swift
@@ -16,24 +16,40 @@ class WhatIsSafeViewController: UIViewController {
     @IBOutlet weak var highlight2: UILabel!
     @IBOutlet weak var paragraphTitle: UILabel!
     @IBOutlet weak var secondParagraph: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
 
+    var factory: ClaimSafeTokenFlowFactory!
+
+    private var completion: (() -> Void)?
+
+    convenience init(completion: @escaping () -> ()) {
+        self.init()
+        self.completion = completion
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        ViewControllerFactory.addCloseButton(self)
         ViewControllerFactory.makeTransparentNavigationBar(self)
+        ViewControllerFactory.addCloseButton(self)
 
+        screenTitle.text = "What is Safe?"
         screenTitle.setStyle(.claimTitle)
-
-        screenTitle.text = "What is the safe?"
 
         firstParagraph.setStyle(.secondary)
         firstParagraph.text = "Safe is critical infrastructure for web3.  It is a programmable account standard that enables secure management of digital assets, data and identity.\nWith this token launch, Safe is now a community-driven ownership platform."
 
-        paragraphTitle.setStyle(.secondary)
         paragraphTitle.text = "Why are we launching a token?"
+        paragraphTitle.setStyle(.title5)
 
         secondParagraph.setStyle(.secondary)
         secondParagraph.text = "As critical web3 infrastructure, Safe needs to be a community-owned, censorship resistant project, with a committed ecosystem stewarding its decisions. A governance token is needed to help coordinate this effort."
+
+        nextButton.setText("Next", .filled)
+
     }
+
+    @IBAction func nextClicked(_ sender: Any) {
+        print("Next button clicked")
+    }
+
 }
