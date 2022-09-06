11958a74b5fe10ed55d597d9202050623d23887e
Dirk Jäckel <dirk@novoda.com>
2022-09-06 08:55:52 +0200

    GH-2588 Show back button and stepnumber

--- Multisig/UI/ClaimToken/Tutorial/WhatIsSafeViewController.swift
+++ Multisig/UI/ClaimToken/Tutorial/WhatIsSafeViewController.swift
@@ -18,19 +18,27 @@ class WhatIsSafeViewController: UIViewController {
     @IBOutlet weak var secondParagraph: UILabel!
     @IBOutlet weak var nextButton: UIButton!
 
+    private var stepLabel: UILabel!
+
+    private var onNext: (() -> ())?
+    private var stepNumber: Int = 1
+    private var maxSteps: Int = 4
+
     var factory: ClaimSafeTokenFlowFactory!
 
     private var completion: (() -> Void)?
 
-    convenience init(completion: @escaping () -> ()) {
-        self.init()
-        self.completion = completion
+    convenience init(stepNumber: Int = 1, maxSteps: Int = 4, onNext: @escaping () -> ()) {
+        self.init(namedClass: WhatIsSafeViewController.self)
+        self.stepNumber = stepNumber
+        self.maxSteps = maxSteps
+        self.completion = onNext
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        ViewControllerFactory.makeTransparentNavigationBar(self)
-        ViewControllerFactory.addCloseButton(self)
+        // TODO How to get the back button and not line beween navbar and content?
+//        ViewControllerFactory.makeTransparentNavigationBar(self)
 
         screenTitle.text = "What is Safe?"
         screenTitle.setStyle(.claimTitle)
@@ -45,6 +53,12 @@ class WhatIsSafeViewController: UIViewController {
         secondParagraph.text = "As critical web3 infrastructure, Safe needs to be a community-owned, censorship resistant project, with a committed ecosystem stewarding its decisions. A governance token is needed to help coordinate this effort."
 
         nextButton.setText("Next", .filled)
+
+        stepLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 21))
+        stepLabel.textAlignment = .right
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: stepLabel)
+        stepLabel.setStyle(.tertiary)
+        stepLabel.text = "\(stepNumber) of \(maxSteps)"
 
     }
 
