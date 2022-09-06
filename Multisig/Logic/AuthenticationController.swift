1b4bf0c87e7b6d74f05871833eda8d28385257d4
Mouaz <Mouaz.alzahabi@gnosis.pm>
2022-09-06 16:08:59 +0200

    Fix backward compatibility

--- Multisig/Logic/AuthenticationController.swift
+++ Multisig/Logic/AuthenticationController.swift
@@ -64,7 +64,9 @@ class AuthenticationController {
     func isPasscodeCorrect(plaintextPasscode: String) throws -> Bool {
         guard let user = user else { return false }
         let password = derivedKey(from: plaintextPasscode)
-        return try accessService.verifyPassword(userID: user.id, password: password)
+        let oldPassword = derivedKey(from: plaintextPasscode, useOldSalt: true)
+        return try accessService.verifyPassword(userID: user.id, password: password) || accessService.verifyPassword(userID: user.id
+                                                                                                                     , password: oldPassword)
     }
 
     /// Deletes the stored passcode. If passcode not set, this operation
@@ -111,9 +113,9 @@ class AuthenticationController {
         return !user.encryptedPassword.isEmpty
     }
 
-    private func derivedKey(from plaintext: String) -> String {
+    private func derivedKey(from plaintext: String, useOldSalt: Bool = false) -> String {
         // For backward compatibility we can't remove Gnosis from this position for now
-        let salt = "Gnosis Safe Multisig Passcode Salt"
+        let salt = salt(oldSalt: useOldSalt)
         var derivedKey = [UInt8](repeating: 0, count: 256 / 8)
         let result = CCKeyDerivationPBKDF(
             CCPBKDFAlgorithm(kCCPBKDF2),
@@ -130,6 +132,10 @@ class AuthenticationController {
             return plaintext
         }
         return Data(derivedKey).toHexString()
+    }
+
+    private func salt(oldSalt: Bool = false) -> String {
+        oldSalt ? "Gnosis Safe Multisig Passcode Salt" : "Safe Multisig Passcode Salt"
     }
 
 
