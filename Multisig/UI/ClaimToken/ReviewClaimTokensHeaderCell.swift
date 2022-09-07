ac1525773702eff40bdfb19a1b2ee3534aa17dfe
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2022-09-07 13:42:35 +0200

    gh-2553 New header for review screen

---
+++ Multisig/UI/ClaimToken/ReviewClaimTokensHeaderCell.swift
@@ -0,0 +1,87 @@
+//
+//  ReviewClaimTokensHeaderCell.swift
+//  Multisig
+//
+//  Created by Dmitry Bespalov on 07.09.22.
+//  Copyright © 2022 Gnosis Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ReviewClaimTokensHeaderCell: UITableViewCell {
+    @IBOutlet private weak var amountView: ClaimAmountView!
+    @IBOutlet private weak var delegateView: AddressInfoView!
+    @IBOutlet private weak var fromView: AddressInfoView!
+    @IBOutlet private weak var toView: AddressInfoView!
+
+    var tokenIcon: UIImage? {
+        get { amountView.image }
+        set { amountView.image = newValue }
+    }
+
+    var amount: String? {
+        get { amountView.amount }
+        set { amountView.amount = newValue }
+    }
+
+    var showsDelegate: Bool = true {
+        didSet {
+            delegateView.isHidden = !showsDelegate
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        delegateView.setTitle("Delegating voting power to:")
+        fromView.setTitle("From:")
+        toView.setTitle("Interact with:")
+    }
+
+    func setDelegate(guardian: Guardian?, address: Address?, chain: Chain) {
+        if let address = address {
+            set(address: address, chain: chain, in: delegateView)
+        } else if let guardian = guardian {
+            delegateView.setAddress(
+                guardian.address.address,
+                ensName: guardian.ens,
+                label: guardian.name,
+                imageUri: guardian.imageURL,
+                browseURL: chain.browserURL(address: guardian.address.description),
+                prefix: chain.shortName
+            )
+        } else {
+            assertionFailure("Missing a value")
+        }
+    }
+
+    func setFrom(address: Address, chain: Chain) {
+        set(address: address, chain: chain, in: fromView)
+    }
+
+    func setTo(address: Address, chain: Chain) {
+        set(address: address, chain: chain, in: toView)
+    }
+
+    func setTo(info: SCGModels.AddressInfo, chain: Chain) {
+        set(info: info, chain: chain, in: toView)
+    }
+
+    private func set(info: SCGModels.AddressInfo, chain: Chain, in view: AddressInfoView) {
+        let (name, image) = NamingPolicy.name(for: info, chainId: chain.id!)
+        view.setAddress(
+            info.value.address,
+            label: name, imageUri: image,
+            browseURL: chain.browserURL(address: info.value.description),
+            prefix: chain.shortName)
+    }
+
+    private func set(address: Address, chain: Chain, in view: AddressInfoView) {
+        let (name, image) = NamingPolicy.name(for: address, chainId: chain.id!)
+        view.setAddress(
+            address,
+            label: name,
+            imageUri: image,
+            browseURL: chain.browserURL(address: address.checksummed),
+            prefix: chain.shortName)
+    }
+}
