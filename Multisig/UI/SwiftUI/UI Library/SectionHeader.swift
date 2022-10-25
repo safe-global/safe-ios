c0559afb434849699ada29907cd07e29cb2ca03a
Mouaz <Mouaz.alzahabi@gnosis.pm>
2022-10-25 16:49:11 +0200

    Fix toggle style

--- Multisig/UI/SwiftUI/UI Library/SectionHeader.swift
+++ Multisig/UI/SwiftUI/UI Library/SectionHeader.swift
@@ -27,7 +27,7 @@ struct SectionHeader: View {
             Spacer()
         }
         .frame(height: text.isEmpty ? 28 : 44)
-        .background(Color.backgroundPrimary)
+        .background(Color.clear)
         .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
     }
 }
