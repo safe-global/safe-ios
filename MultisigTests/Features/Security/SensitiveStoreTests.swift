d2972821a4a328dc322cbe4ecf5f5670e8fa353e
Dirk Jäckel <dirk@safe.global>
2023-01-11 15:14:19 +0000

    GH-2827 Test stores  do not overwrite each other

--- MultisigTests/Features/Security/SensitiveStoreTests.swift
+++ MultisigTests/Features/Security/SensitiveStoreTests.swift
@@ -9,55 +9,57 @@ import LocalAuthentication
 
 public class SensitiveStoreTests: XCTestCase {
 
-    var encryptedStore: EncryptedStore! = nil
+    var sensitiveKeyStore: EncryptedStore! = nil
+    var dataKeyStore: EncryptedStore! = nil
     var keychainItemStore: KeychainItemStore! = nil
 
     public override func setUp() {
         super.setUp()
         keychainItemStore = KeychainItemStore(KeychainStore())
-        encryptedStore = SensitiveStore(protectionClass: .sensitive, keychainItemStore)
+        sensitiveKeyStore = SensitiveStore(protectionClass: .sensitive, keychainItemStore)
+        dataKeyStore = SensitiveStore(protectionClass: .data, keychainItemStore)
     }
 
     public override func tearDown() {
         super.tearDown()
-        try! encryptedStore.deleteAllKeys()
+        try! sensitiveKeyStore.deleteAllKeys()
     }
 
     func testInitialSetup() {
-        XCTAssertEqual(encryptedStore.isInitialized(), false)
+        XCTAssertEqual(sensitiveKeyStore.isInitialized(), false)
         do {
-            try encryptedStore.initializeKeyStore()
+            try sensitiveKeyStore.initializeKeyStore()
         } catch {
             XCTFail() // do not throw
         }
-        XCTAssertEqual(encryptedStore.isInitialized(), true)
+        XCTAssertEqual(sensitiveKeyStore.isInitialized(), true)
     }
 
     func testImport() throws {
         let randomKey = Data(ethHex: "da18066dda40499e6ef67a392eda0fd90acf804448a765db9fa9b6e7dd15c322") as EthPrivateKey
-        try encryptedStore.initializeKeyStore()
+        try sensitiveKeyStore.initializeKeyStore()
 
-        try encryptedStore.import(id: DataID(id:"0xE86935943315293154c7AD63296b4e1adAc76364"), ethPrivateKey: randomKey)
+        try sensitiveKeyStore.import(id: DataID(id:"0xE86935943315293154c7AD63296b4e1adAc76364"), ethPrivateKey: randomKey)
 
-        let result = try encryptedStore.find(dataID: DataID(id: "0xE86935943315293154c7AD63296b4e1adAc76364"), password: nil)
+        let result = try sensitiveKeyStore.find(dataID: DataID(id: "0xE86935943315293154c7AD63296b4e1adAc76364"), password: nil)
         XCTAssertEqual(result?.toHexString(), randomKey.toHexString())
     }
 
     func testEthereumKeyNotFound() throws {
-        try encryptedStore.initializeKeyStore()
+        try sensitiveKeyStore.initializeKeyStore()
 
-        let ethPrivateKey = try encryptedStore.find(dataID: DataID(id: "0xfb1ca734579C3F2dC6DC8cD64A4f5D91891387C6"), password: nil)
+        let ethPrivateKey = try sensitiveKeyStore.find(dataID: DataID(id: "0xfb1ca734579C3F2dC6DC8cD64A4f5D91891387C6"), password: nil)
         XCTAssertEqual(ethPrivateKey, nil)
     }
 
     func testChangePassword() {
-        XCTAssertEqual(encryptedStore.isInitialized(), false)
+        XCTAssertEqual(sensitiveKeyStore.isInitialized(), false)
         do {
-            try encryptedStore.initializeKeyStore()
+            try sensitiveKeyStore.initializeKeyStore()
 
-            try encryptedStore.changePassword(from: nil, to: "test123", useBiometry: false)
-            try encryptedStore.changePassword(from: "test123", to: "random", useBiometry: false)
-            try encryptedStore.changePassword(from: "random", to: nil, useBiometry: false)
+            try sensitiveKeyStore.changePassword(from: nil, to: "test123", useBiometry: false)
+            try sensitiveKeyStore.changePassword(from: "test123", to: "random", useBiometry: false)
+            try sensitiveKeyStore.changePassword(from: "random", to: nil, useBiometry: false)
 
         } catch {
             XCTFail() // should not throw
@@ -68,16 +70,16 @@ public class SensitiveStoreTests: XCTestCase {
         guard simulatorCheck() else {
             throw XCTSkip("Test not supported on simulator")
         }
-        XCTAssertEqual(encryptedStore.isInitialized(), false)
+        XCTAssertEqual(sensitiveKeyStore.isInitialized(), false)
         do {
-            try encryptedStore.initializeKeyStore()
-            try encryptedStore.changePassword(from: nil, to: "test123", useBiometry: false)
+            try sensitiveKeyStore.initializeKeyStore()
+            try sensitiveKeyStore.changePassword(from: nil, to: "test123", useBiometry: false)
         } catch {
             XCTFail() // should not throw
         }
 
         do {
-            try encryptedStore.changePassword(from: "wrong", to: "random", useBiometry: false)
+            try sensitiveKeyStore.changePassword(from: "wrong", to: "random", useBiometry: false)
             XCTFail() // should've thrown
         } catch {
             // check for correct exception?
@@ -88,20 +90,48 @@ public class SensitiveStoreTests: XCTestCase {
         guard simulatorCheck() else {
             throw XCTSkip("Test not supported on simulator")
         }
-        XCTAssertEqual(encryptedStore.isInitialized(), false)
+        XCTAssertEqual(sensitiveKeyStore.isInitialized(), false)
         do {
-            try encryptedStore.initializeKeyStore()
+            try sensitiveKeyStore.initializeKeyStore()
 
-            try encryptedStore.changePassword(from: nil, to: nil, useBiometry: true)
+            try sensitiveKeyStore.changePassword(from: nil, to: nil, useBiometry: true)
 
             // check ?
-            try encryptedStore.changePassword(from: nil, to: "test123", useBiometry: false)
-            try encryptedStore.changePassword(from: "test123", to: "random", useBiometry: false)
-            try encryptedStore.changePassword(from: "random", to: nil, useBiometry: false)
+            try sensitiveKeyStore.changePassword(from: nil, to: "test123", useBiometry: false)
+            try sensitiveKeyStore.changePassword(from: "test123", to: "random", useBiometry: false)
+            try sensitiveKeyStore.changePassword(from: "random", to: nil, useBiometry: false)
 
         } catch {
             XCTFail() // should not throw
         }
+    }
+
+    func testImportDataKey() throws {
+        let randomKey = Data(ethHex: "da18066dda40499e6ef67a392eda0fd90acf804448a765db9fa9b6e7dd15c322") as EthPrivateKey
+        try dataKeyStore.initializeKeyStore()
+
+        try dataKeyStore.import(id: DataID(id:"0xE86935943315293154c7AD63296b4e1adAc76364"), ethPrivateKey: randomKey)
+
+        let result = try dataKeyStore.find(dataID: DataID(id: "0xE86935943315293154c7AD63296b4e1adAc76364"), password: nil)
+        XCTAssertEqual(result?.toHexString(), randomKey.toHexString())
+    }
+
+    func testImportDataAndSensitiveKeyStore() throws {
+        let randomSensitiveKey = Data(ethHex: "da18066dda40499e6ef67a392eda0fd90acf804448a765db9fa9b6e7dd15c322") as EthPrivateKey
+        try sensitiveKeyStore.initializeKeyStore()
+
+        let randomDataKey = Data(ethHex: "cb18066dda40499e6ef67a392eda0fd90acf804448a765db9fa9b6e7dd15c321") as EthPrivateKey
+        try dataKeyStore.initializeKeyStore()
+
+        try sensitiveKeyStore.import(id: DataID(id:"0xE86935943315293154c7AD63296b4e1adAc76364"), ethPrivateKey: randomSensitiveKey)
+        try dataKeyStore.import(id: DataID(id:"0xE86935943315293154c7AD63296b4e1adAc76364"), ethPrivateKey: randomDataKey)
+
+        let result = try sensitiveKeyStore.find(dataID: DataID(id: "0xE86935943315293154c7AD63296b4e1adAc76364"), password: nil)
+        XCTAssertEqual(result?.toHexString(), randomSensitiveKey.toHexString())
+
+        let result2 = try dataKeyStore.find(dataID: DataID(id: "0xE86935943315293154c7AD63296b4e1adAc76364"), password: nil)
+        XCTAssertEqual(result2?.toHexString(), randomDataKey.toHexString())
+
     }
 
     private func simulatorCheck() -> Bool {
