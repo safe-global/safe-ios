37eebdf5bba6e543c759a777a65742b74c89305d
Vitaly Katz <vitaly@gnosis.io>
2023-01-12 10:51:41 +0100

    gh-2892 Fix typo

--- Multisig/Data/Services/Utils/GSError.swift
+++ Multisig/Data/Services/Utils/GSError.swift
@@ -636,8 +636,8 @@ enum GSError {
     }
 
     struct RequiredPasscode: DetailedLocalizedError {
-        let description = "Faild to get passcode"
-        let reason = "Your passcode is required to perfom this operation"
+        let description = "Failed to get passcode"
+        let reason = "Your passcode is required to perform this operation"
         let howToFix = "Enter correct passcode and try again"
         let domain = iOSErrorDomain
         let code = 1325
