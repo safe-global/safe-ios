486b7c834f0f563f4affd9cf9fbf644ea0bdbca0
Mouaz <mouaz@safe.global>
2023-03-01 11:46:50 +0100

    Fix compilation issue

--- Multisig/UI/Settings/OwnerKeyManagement/KeystoneOwnerKey/KeystoneSignFlow.swift
+++ Multisig/UI/Settings/OwnerKeyManagement/KeystoneOwnerKey/KeystoneSignFlow.swift
@@ -68,8 +68,8 @@ final class KeystoneSignFlow: UIFlow {
 extension KeystoneSignFlow: QRCodeScannerViewControllerDelegate {
  func scannerViewControllerDidScan(_ code: String) {
      guard
-        let signature = URRegistry.shared.getSignature(from: code)?.signature,
-        let unmarshaledSignature = SECP256K1.UnmarshaledSignature(
+         let signature = URRegistry.shared.getSignature(from: code),
+         let unmarshaledSignature = SECP256K1.UnmarshaledSignature(
             keystoneSignature: signature,
             isLegacyTx: signInfo.signType == .transaction,
             chainId: signInfo.chain?.id ?? "0")
