fe73ecced46a61b27881150ee6e26e70518a4dc2
Vitaly Katz <vitaly@gnosis.io>
2023-03-09 14:49:36 +0100

    gh-2931 What is relaying screen layout

--- Multisig/UI/Assets/RelayOnboarding/ROWhatIsViewController.swift
+++ Multisig/UI/Assets/RelayOnboarding/ROWhatIsViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class ROWhatIsViewController: UIViewController {
 
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var paragraph1Label: UILabel!
+    @IBOutlet private weak var paragraph2TitleLabel: UILabel!
+    @IBOutlet private weak var paragraph2Label: UILabel!
+    @IBOutlet private weak var paragraph3TitleLabel: UILabel!
+    @IBOutlet private weak var paragraph3Label: UILabel!
+    @IBOutlet private weak var paragraph4Label: UILabel!
+    @IBOutlet private weak var nextButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        ViewControllerFactory.addCloseButton(self)
+        ViewControllerFactory.removeNavigationBarBorder(self)
+        //navigationItem.largeTitleDisplayMode = .never
+
+        titleLabel.setStyle(.title1)
+        paragraph1Label.setStyle(.subheadlineSecondary)
+        paragraph2TitleLabel.setStyle(.headline)
+        paragraph2Label.setStyle(.subheadlineSecondary)
+        paragraph3TitleLabel.setStyle(.headline)
+        paragraph3Label.setStyle(.subheadlineSecondary)
+        paragraph4Label.setStyle(.subheadlineSecondary)
+
+        nextButton.setText("Next", .filled)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //Tracker.trackEvent()
+    }
+
+    @IBAction func didTapNext(_ sender: Any) {
     }
 }
