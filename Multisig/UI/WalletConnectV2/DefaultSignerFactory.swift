06dfdb31898164f0532ce6f5f57d37fda8196bd8
Dirk Jäckel <dirk@safe.global>
2023-03-20 10:43:30 +0100

    GH-2920 empty implemenetation

--- Multisig/UI/WalletConnectV2/DefaultSignerFactory.swift
+++ Multisig/UI/WalletConnectV2/DefaultSignerFactory.swift
@@ -19,26 +19,18 @@ public struct DefaultSignerFactory: SignerFactory {
 }
 
 public struct Web3Signer: EthereumSigner {
-
+    enum Web3Signer: Error {
+      case shouldNotBeCalledError
+    }
     public func sign(message: Data, with key: Data) throws -> EthereumSignature {
-        let privateKey = try EthereumPrivateKey(privateKey: [UInt8](key))
-        let signature = try privateKey.sign(message: message.bytes)
-        return EthereumSignature(v: UInt8(signature.v), r: signature.r, s: signature.s)
+        throw Web3Signer.shouldNotBeCalledError
     }
 
     public func recoverPubKey(signature: EthereumSignature, message: Data) throws -> Data {
-        let publicKey = try EthereumPublicKey(
-            message: message.bytes,
-            v: EthereumQuantity(quantity: BigUInt(signature.v)),
-            r: EthereumQuantity(signature.r),
-            s: EthereumQuantity(signature.s)
-        )
-        return Data(publicKey.rawPublicKey)
+        throw Web3Signer.shouldNotBeCalledError
     }
 
     public func keccak256(_ data: Data) -> Data {
-        let digest = SHA3(variant: .keccak256)
-        let hash = digest.calculate(for: [UInt8](data))
-        return Data(hash)
+        return Data()
     }
 }
