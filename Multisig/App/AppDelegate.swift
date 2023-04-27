17b4537284be612621e5ee6e9d2f30f116a3753b
Dirk Jäckel <dirk@safe.global>
2023-04-27 09:29:54 +0200

    gh-3055 Bump Intercom lib to 14.+ (#3082)
    * gh-3055 Bump Intercom lib to 14.+
    
    * gh-3055 Remove unneeded import. Show error when devicetoken cannot be set in Intercom
    
    * gh-3055 Make everything static
    
    * gh-3055 Remove intercom instance

--- Multisig/App/AppDelegate.swift
+++ Multisig/App/AppDelegate.swift
@@ -9,12 +9,11 @@
 import UIKit
 import SwiftUI
 import Firebase
-import Intercom
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         App.shared.firebaseConfig.setUp()
-        App.shared.intercomConfig.setUp()
+        IntercomConfig.setUp()
 
         UIApplication.shared.registerForRemoteNotifications()
 
@@ -86,16 +85,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         LogService.shared.debug("PUSH: Received APNS token: \(deviceToken.toHexStringWithPrefix())")
         Messaging.messaging().apnsToken = deviceToken
-        Intercom.setDeviceToken(deviceToken)
+        IntercomConfig.setDeviceToken(deviceToken) { error in
+            // this will fail before a Safe was added
+        }
     }
 
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         LogService.shared.debug("PUSH: didReceiveRemoteNotification with userInfo: \(userInfo)")
         Messaging.messaging().appDidReceiveMessage(userInfo)
 
-        if Intercom.isIntercomPushNotification(userInfo) {
+        if IntercomConfig.isIntercomPushNotification(userInfo) {
             LogService.shared.debug("PUSH: didReceiveRemoteNotification Intercom push notification with userInfo: \(userInfo)")
-            App.shared.intercomConfig.pushNotificationUserInfo = userInfo
+            IntercomConfig.pushNotificationUserInfo = userInfo
         }
 
         completionHandler(.noData)
