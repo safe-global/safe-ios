e41405f291780da6592483b91680ccf686c1077b
Mouaz <mouaz@safe.global>
2023-05-08 17:27:00 +0200

    Remove what is new screen

--- Multisig/UI/App/WhatsNewHandler.swift
+++ Multisig/UI/App/WhatsNewHandler.swift
@@ -11,41 +11,43 @@ class WhatsNewHandler {
     var whatsNewViewController: WhatsNewViewController?
 
     init() {
-        let whatsNew = WhatsNew(
-            version: "3.19.0",
-            title: "What's new",
-            features: [
-                WhatsNew.Feature(
-                    image: WhatsNew.Feature.Image(name: "ico-whats-new-relayer", bundle: .main, renderingMode: .original, foregroundColor: nil),
-                    title: "Gasless transactions",
-                    subtitle: "Transact without paying gas. Safes on Gnosis Chain receive 5 free transactions per hour for a limited time. Try it out on Gnosis Chain!"
-                ),
-                WhatsNew.Feature(
-                    image: WhatsNew.Feature.Image(name: "ico-whats-new-wc2", bundle: .main, renderingMode: .original, foregroundColor: nil),
-                    title: "WalletConnect V2.0",
-                    subtitle: "Support for WalletConnect V2.0 for easier wallet connection."
-                )
-            ],
-            primaryAction: WhatsNew.PrimaryAction(
-                title: "Let's go",
-                backgroundColor: .primary,
-                foregroundColor: .backgroundPrimary,
-                hapticFeedback: .notification(.success),
-                onDismiss: nil
-            ),
-            secondaryAction: nil
-        )
-
-        // for debugging, use the In-memory store version
-        let versionStore: WhatsNewVersionStore = UserDefaultsWhatsNewVersionStore()
-//         let versionStore: WhatsNewVersionStore = InMemoryWhatsNewVersionStore()
-
-        let layout = WhatsNew.Layout(featureImageWidth: 60, featureHorizontalAlignment: .top)
-
-        whatsNewViewController = WhatsNewViewController(
-            whatsNew: whatsNew,
-            versionStore: versionStore,
-            layout: layout
-        )
+        whatsNewViewController = nil
+        // TODO: Uncomment and change on new releases
+//        let whatsNew = WhatsNew(
+//            version: "3.19.0",
+//            title: "What's new",
+//            features: [
+//                WhatsNew.Feature(
+//                    image: WhatsNew.Feature.Image(name: "ico-whats-new-relayer", bundle: .main, renderingMode: .original, foregroundColor: nil),
+//                    title: "Gasless transactions",
+//                    subtitle: "Transact without paying gas. Safes on Gnosis Chain receive 5 free transactions per hour for a limited time. Try it out on Gnosis Chain!"
+//                ),
+//                WhatsNew.Feature(
+//                    image: WhatsNew.Feature.Image(name: "ico-whats-new-wc2", bundle: .main, renderingMode: .original, foregroundColor: nil),
+//                    title: "WalletConnect V2.0",
+//                    subtitle: "Support for WalletConnect V2.0 for easier wallet connection."
+//                )
+//            ],
+//            primaryAction: WhatsNew.PrimaryAction(
+//                title: "Let's go",
+//                backgroundColor: .primary,
+//                foregroundColor: .backgroundPrimary,
+//                hapticFeedback: .notification(.success),
+//                onDismiss: nil
+//            ),
+//            secondaryAction: nil
+//        )
+//
+//        // for debugging, use the In-memory store version
+//        let versionStore: WhatsNewVersionStore = UserDefaultsWhatsNewVersionStore()
+////         let versionStore: WhatsNewVersionStore = InMemoryWhatsNewVersionStore()
+//
+//        let layout = WhatsNew.Layout(featureImageWidth: 60, featureHorizontalAlignment: .top)
+//
+//        whatsNewViewController = WhatsNewViewController(
+//            whatsNew: whatsNew,
+//            versionStore: versionStore,
+//            layout: layout
+//        )
     }
 }
