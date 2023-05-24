9353643394aa80a4679f982756d851c13bf779fd
Mouaz <mouaz@safe.global>
2023-05-24 17:11:46 +0200

    Fix typo

--- Multisig/UI/Settings/Passcode/SecuritySettingsViewController.swift
+++ Multisig/UI/Settings/Passcode/SecuritySettingsViewController.swift
@@ -78,7 +78,7 @@ class SecuritySettingsViewController: UITableViewController {
         if isPasscodeSet {
             data = [(section: .lockMethod, rows: [.enableSecurityLock, .lockMethod])]
 
-            if !AppConfiguration.FeatureToggles.securityCenter || 9lock.isPasscodeRequired() {
+            if !AppConfiguration.FeatureToggles.securityCenter || lock.isPasscodeRequired() {
                 data.append((section: .passcode, rows: [.changePasscode]))
             }
 
