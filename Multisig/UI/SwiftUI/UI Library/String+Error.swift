2c2697ee492126ade6fb4ef40b4d91d449c46afc
Mouaz <mouaz@safe.global>
2023-06-15 12:56:52 +0200

    Fix compilation issue

---
+++ Multisig/UI/SwiftUI/UI Library/String+Error.swift
@@ -0,0 +1,15 @@
+//
+//  String+Error.swift .swift
+//  Multisig
+//
+//  Created by Mouaz on 6/15/23.
+//  Copyright © 2023 Gnosis Ltd. All rights reserved.
+//
+
+import Foundation
+
+extension String: LocalizedError {
+    public var errorDescription: String? {
+        self
+    }
+}
