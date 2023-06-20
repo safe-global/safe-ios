5b636e26c65f72579e5cb23ef0d262d95c9396b9
Mouaz <mouaz@safe.global>
2023-06-20 17:32:53 +0200

    implement CreateSafeWithSocialIntroViewController

--- Multisig/UI/Safe Management/Create Safe/CreateSafeWithSocialIntroViewController.swift
+++ Multisig/UI/Safe Management/Create Safe/CreateSafeWithSocialIntroViewController.swift
@@ -9,22 +9,33 @@
 import UIKit
 
 class CreateSafeWithSocialIntroViewController: UIViewController {
+    @IBOutlet private weak var ribbonView: RibbonView!
+    @IBOutlet private weak var googleButton: UIButton!
+    @IBOutlet private weak var appleButton: UIButton!
+    @IBOutlet private weak var addressButton: UIButton!
+    @IBOutlet private weak var orLabel: UILabel!
+    @IBOutlet private weak var headerLabel: UILabel!
+    @IBOutlet private weak var infoView3: InfoView!
+    @IBOutlet private weak var infoView2: InfoView!
+    @IBOutlet private weak var infoView1: InfoView!
 
+    var chain: SCGModels.Chain!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.title = "Create a Safe Account"
+        ribbonView.update(scgChain: chain)
+        orLabel.setStyle(.caption1)
+        appleButton.setText("Continue with Apple ID", .filled)
+        googleButton.setText("Continue with Google", .filled)
+        addressButton.setText("Continue with a wallet address", .bordered)
+        infoView1.set(text: "Create a Safe Account now and add more owners later for better security")
+        infoView2.set(text: "Owner key is protected by your social login")
+        infoView3.set(text: "No need to keep seed phrases")
+        headerLabel.hyperLinkLabel("Select a social login to create your Safe Account. ",
+                                   prefixStyle: .headlineSecondary,
+                                   linkText: "How does it work?",
+                                   linkStyle: .body,
+                                   linkIcon: nil,
+                                   underlined: false)
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
