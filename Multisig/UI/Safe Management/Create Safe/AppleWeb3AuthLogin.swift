664acdad664a35167c7c79fee0d85f600bf39417
Dirk Jäckel <dirk@safe.global>
2023-07-11 11:07:38 +0200

    GH-3138 apple id aggregate verifier commit

--- Multisig/UI/Safe Management/Create Safe/AppleWeb3AuthLogin.swift
+++ Multisig/UI/Safe Management/Create Safe/AppleWeb3AuthLogin.swift
@@ -43,13 +43,69 @@ extension AppleWeb3AuthLogin: ASAuthorizationControllerDelegate {
                         loglevel: .debug
                 )
 
-                let data = try await tdsdk.getTorusKey(verifier: App.configuration.web3auth.appleVerifier,
+                let data = try await tdsdk.getAggregateTorusKey(verifier: App.configuration.web3auth.appleVerifier,
                         verifierId: sub,
-                        idToken: token
+                        idToken: token,
+                        subVerifierDetails: SubVerifierDetails(
+                                loginType: .web,
+                                loginProvider: .jwt,
+                                clientId: "",
+                                //clientId: App.configuration.web3auth.appleClientId,
+                                verifier: App.configuration.web3auth.appleVerifier,
+                                redirectURL: "")
                 )
 
+
+
+                /// Retrieve the Torus key from the nodes given an already known token. Useful if a custom aggregate login flow is required.
+                /// - Parameters:
+                ///   - verifier: A verifier is a unique identifier for your OAuth registration on the torus network. The public/private keys generated for a user are scoped to a verifier.
+                ///   - verifierId: The unique identifier to publicly represent a user on a verifier. e.g: email, sub etc. other fields can be classified as verifierId,
+                ///   - subVerifierDetails: An array of verifiers to be used for the aggregate login flow, with their respective token and verifier name.
+                /// - Returns: A promise that resolve with a Dictionary that contain at least `privateKey` and `publicAddress` field..
+                /// open func getAggregateTorusKey(verifier: String,
+                /// verifierId: String,
+                /// idToken: String,
+                /// subVerifierDetails: SubVerifierDetails,
+                /// userData: [String: Any] = [:]) async throws -> [String: Any] {
+
+
+//                public struct SubVerifierDetails {
+//                    public let loginType: SubVerifierType
+//                    public let clientId: String
+//                    public let verifier: String
+//                    public let loginProvider: LoginProviders
+//                    public let redirectURL: String
+//                    public let handler: AbstractLoginHandler
+//                    public var urlSession: URLSession
+//
+//                    public enum codingKeys: String, CodingKey {
+//                        case clientId
+//                        case loginProvider
+//                        case subVerifierId
+//                    }
+//
+//                    public init(loginType: SubVerifierType = .web,
+//            loginProvider: LoginProviders,
+//            clientId: String,
+//            verifier: String,
+//            redirectURL: String,
+//            browserRedirectURL: String? = nil,
+//            jwtParams: [String: String] = [:],
+//            urlSession: URLSession = URLSession.shared) {
+//                        self.loginType = loginType
+//                        self.clientId = clientId
+//                        self.loginProvider = loginProvider
+//                        self.verifier = verifier
+//                        self.redirectURL = redirectURL
+//                        self.urlSession = urlSession
+//                        handler = self.loginProvider.getHandler(loginType: loginType, clientID: self.clientId, redirectURL: self.redirectURL, browserRedirectURL: browserRedirectURL, jwtParams: jwtParams, urlSession: urlSession)
+//                    }
+
+
+
                 await MainActor.run(body: {
-                    dump(data, name: "---> getTorusKey() result: ")
+                    dump(data, name: "---> getAggregateTorusKey() result: ")
                     // App.shared.snackbar.show(message: "Private Key: \(data["privateKey"] as? String)")
                     onClose()
                 })
