9055ef20367f408e72f6cfc879265403c10984bc
Vitaly Katz <vitaly@gnosis.io>
2023-07-13 16:51:33 +0200

    gh-3141 Add tracking for social login actions

--- Multisig/Cross-layer/Analytics/TrackingEvent.swift
+++ Multisig/Cross-layer/Analytics/TrackingEvent.swift
@@ -433,6 +433,17 @@ enum TrackingEvent: String, Trackable {
     case userClaimNotDao                            = "user_claim_not_dao"
     case userClaimNotForum                          = "user_claim_not_forum"
     case userClaimNotOk                             = "user_claim_not_ok"
+
+    // MARK: Web2 style onboarding
+    case screenStartingInfo                        = "screen_starting_screen_info"
+    case screenSocialLoginInfo                     = "screen_info_modal_sheet"
+
+    case userHowItWorks                            = "user_how_does_it_work"
+    case userContinueGoogle                        = "user_continue_google"
+    case userContinueApple                         = "user_continue_apple"
+    case userContinueAddress                       = "user_use_wallet_address"
+    case userReadMore                              = "user_read_more"
+
 }
 
 extension TrackingEvent {
