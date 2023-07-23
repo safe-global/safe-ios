feb647bd7990db327dbca98fc014aad0fffbe7a6
Dirk Jäckel <dirk@safe.global>
2023-07-23 15:12:53 +0200

    Revert "refs/heads/back-to-universal-links WIP"
    This reverts commit ca2e07ee2a39dda570813f96f09a438e014d6304.

--- Multisig/UI/Safe Management/Create Safe/GoogleWeb3AuthLoginModel.swift
+++ Multisig/UI/Safe Management/Create Safe/GoogleWeb3AuthLoginModel.swift
@@ -3,96 +3,47 @@ import CustomAuth
 import UIKit
 
 class GoogleWeb3AuthLoginModel {
-    var onClose: ((_ key: String?, _ email: String?) -> Void)
+    var authorizationComplete: () -> Void
+    var keyGenerationComplete: ((_ key: String, _ email: String?) -> Void)
 
-    init(onClose: @escaping ((_ key: String?, _ email: String?) -> Void)) {
-        self.onClose = onClose
+    init(authorizationComplete: @escaping () -> Void, keyGenerationComplete: @escaping ((_ key: String, _ email: String?) -> Void)) {
+        self.authorizationComplete = authorizationComplete
+        self.keyGenerationComplete = keyGenerationComplete
     }
 
     static func handle(url: URL) -> Bool {
-
-
-        LogService.shared.debug("-----> handle(url: URL): \(url.absoluteString)")
-       // if url.absoluteString.starts(with: App.configuration.web3auth.redirectUrl) {
+        if url.absoluteString.starts(with: App.configuration.web3auth.redirectScheme + "://") {
             CustomAuth.handle(url: url)
             return true
-        //}
-        //return false
+        }
+        return false
     }
 
-    func loginWithCustomAuth(caller: UIViewController) {
+    func loginWithCustomAuth() {
         Task {
-            let sub = SubVerifierDetails(loginType: .web,
+            let sub = SubVerifierDetails(loginType: .installed,
                                          loginProvider: .google,
                                          clientId: App.configuration.web3auth.googleClientId,
                                          verifier: App.configuration.web3auth.googleVerifier,
-                                         redirectURL: App.configuration.web3auth.redirectUrl
+                                         redirectURL: App.configuration.web3auth.redirectScheme + "://"
             )
             let tdsdk = CustomAuth(aggregateVerifierType: .singleLogin,
                                    aggregateVerifier: App.configuration.web3auth.googleVerifier,
                                    subVerifierDetails: [sub],
                                    network: .CYAN
             )
-            let data = try await tdsdk.triggerLogin(controller: caller)
+            await MainActor.run(body: {
+                authorizationComplete()
+            })
+
+            let data = try await tdsdk.triggerLogin()
             await MainActor.run(body: {
                 let key = data["privateKey"] as? String
                 let userInfo = data["userInfo"] as? Dictionary ?? [:] as Dictionary
                 let email = userInfo["email"] as? String ?? "email withheld"
 
-                onClose(key, email)
-                caller.closeModal()
+                keyGenerationComplete(key!, email)
             })
         }
     }
 }
-
-
-
-
-
-
-//class GoogleWeb3AuthLoginModel {
-//    var authorizationComplete: () -> Void
-//    var keyGenerationComplete: ((_ key: String, _ email: String?) -> Void)
-//
-//    init(authorizationComplete: @escaping () -> Void, keyGenerationComplete: @escaping ((_ key: String, _ email: String?) -> Void)) {
-//        self.authorizationComplete = authorizationComplete
-//        self.keyGenerationComplete = keyGenerationComplete
-//    }
-//
-//    static func handle(url: URL) -> Bool {
-//        if url.absoluteString.starts(with: App.configuration.web3auth.redirectScheme + "://") {
-//            CustomAuth.handle(url: url)
-//            return true
-//        }
-//        return false
-//    }
-//
-//    func loginWithCustomAuth() {
-//        Task {
-//            let sub = SubVerifierDetails(loginType: .installed,
-//                                         loginProvider: .google,
-//                                         clientId: App.configuration.web3auth.googleClientId,
-//                                         verifier: App.configuration.web3auth.googleVerifier,
-//                                         redirectURL: App.configuration.web3auth.redirectScheme + "://"
-//            )
-//            let tdsdk = CustomAuth(aggregateVerifierType: .singleLogin,
-//                                   aggregateVerifier: App.configuration.web3auth.googleVerifier,
-//                                   subVerifierDetails: [sub],
-//                                   network: .CYAN
-//            )
-//            await MainActor.run(body: {
-//                authorizationComplete()
-//            })
-//
-//            let data = try await tdsdk.triggerLogin()
-//            await MainActor.run(body: {
-//                let key = data["privateKey"] as? String
-//                let userInfo = data["userInfo"] as? Dictionary ?? [:] as Dictionary
-//                let email = userInfo["email"] as? String ?? "email withheld"
-//
-//                keyGenerationComplete(key!, email)
-//            })
-//        }
-//    }
-//}
