6d16c914780ac13f6f8b9683d52075d54ea752d7
Mouaz <mouaz@safe.global>
2023-08-01 09:56:36 +0200

    Create MoonpayService

--- Multisig/UI/Assets/Ramper.swift
+++ Multisig/UI/Assets/Ramper.swift
@@ -88,11 +88,15 @@ class Ramper: MoonpayCallbackInterface {
 
     }
 
-    func sigature(address: String, chain: String, theme: String, defaultCurrencyCode: String, environment: String) -> String {
+    private func sigature(address: String, chain: String, theme: String, defaultCurrencyCode: String, environment: String) -> String {
         let bytes = Array( "?apiKey=\(App.configuration.services.moonpayKey)&defaultCurrencyCode=\(defaultCurrencyCode)&walletAddresses=%7B%22\(chain)%22%3A%22\(address)%22%7D&theme=\(theme)&themeId=\(MOONPAY_THEME_ID_SAFE)&language=en&baseCurrencyCode=\(AppSettings.selectedFiatCode)&mpSdk=%7B%22environment%22%3A%22\(environment)%22%2C%22flow%22%3A%22buy%22%2C%22version%22%3A%221.0%22%2C%22platform%22%3A%22iOS%22%7D".utf8)
 
         let key: Array<UInt8> = Array(App.configuration.services.moonpaySecretKey.utf8)
 
         return Data(try! HMAC(key: key, variant: .sha2(.sha256)).authenticate(bytes)).base64EncodedString()
+    }
+
+    private func walletAddresses(chain: Chain) -> String {
+        
     }
 }
