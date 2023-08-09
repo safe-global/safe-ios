95d2f417230c73f255e8a4cf766bc8dbffaf0ce4
Dirk Jäckel <dirk@safe.global>
2023-08-09 17:22:04 +0200

    setup deviceshare, setup password and changePassword are working

--- Multisig/UI/Safe Management/Create Safe/GoogleWeb3AuthLoginModel.swift
+++ Multisig/UI/Safe Management/Create Safe/GoogleWeb3AuthLoginModel.swift
@@ -16,12 +16,13 @@ class GoogleWeb3AuthLoginModel {
         Task {
             let sub = SubVerifierDetails(loginType: .installed,
                                          loginProvider: .google,
-                                         clientId: App.configuration.web3auth.googleClientId,
-                                         verifier: App.configuration.web3auth.googleVerifierSub,
-                                         redirectURL: App.configuration.web3auth.redirectScheme + GoogleWeb3AuthLoginModel.schemePostfix
+                                         clientId: "500572929132-57dbeqrtq84m5oibve186vfmdd6p5rmh.apps.googleusercontent.com",
+                                         verifier: "google-sub-bttr-500572929132",
+                                         redirectURL: "com.googleusercontent.apps.500572929132-57dbeqrtq84m5oibve186vfmdd6p5rmh://"
+
             )
             let tdsdk = CustomAuth(aggregateVerifierType: .singleIdVerifier,
-                                   aggregateVerifier: App.configuration.web3auth.googleVerifierAggregate,
+                                   aggregateVerifier: "google-aggregate-bttr",
                                    subVerifierDetails: [sub],
                                    network: .CYAN,
                                    enableOneKey: true
@@ -40,7 +41,28 @@ class GoogleWeb3AuthLoginModel {
                     if Web3AuthMFAService.mfaEnabled(userInfo: userInfo) {
                         Task {
                             do {
-                                let web3authService = try await Web3AuthMFAService(postBoxKey: key!, publicAddress: publicAddress!, password: "foobar23")
+                                let web3authService = try await Web3AuthMFAService(postBoxKey: key!, publicAddress: publicAddress!)
+                                let finalKey = web3authService.finalKey
+                                LogService.shared.debug("---> finalKey: \(finalKey)")
+
+                                //try await web3authService.setupPassword(password: "foobar23")
+                                try await web3authService.changePassword(oldPassword: "foobar23", newPassword: "foobar2342")
+                                LogService.shared.debug("---> First password change completed")
+                                try await web3authService.changePassword(oldPassword: "foobar2342", newPassword: "foobar23")
+                                LogService.shared.debug("---> Second password change completed")
+
+                                self.keyGenerationComplete(finalKey!, email + "_mfa", nil)
+                            } catch {
+                                LogService.shared.error("---> error: \(error)")
+                                await MainActor.run(body: { [weak self] in
+                                    self?.keyGenerationComplete(nil, nil, error)
+                                })
+                            }
+                        }
+                    } else {
+                        Task {
+                            do {
+                                let web3authService = try await Web3AuthMFAService(postBoxKey: key!, publicAddress: publicAddress!, password: "foobar2342", initialize: true)
                                 let finalKey = web3authService.finalKey
                                 print ("---> finalKey: \(finalKey)")
                                 self.keyGenerationComplete(finalKey!, email + "_mfa", nil)
@@ -51,8 +73,8 @@ class GoogleWeb3AuthLoginModel {
                                 })
                             }
                         }
-                    } else {
-                        self.keyGenerationComplete(key!, email, nil)
+
+                        //self.keyGenerationComplete(key!, email, nil)
                     }
 
                 })
--- Multisig/UI/Safe Management/Create Safe/Web3AuthMFAService.swift
+++ Multisig/UI/Safe Management/Create Safe/Web3AuthMFAService.swift
@@ -9,10 +9,6 @@
 import Foundation
 import tkey_pkg
 
-// activate MFS by either
-// 1. adding the current device as a trusted device or
-// 2. Setting a password
-
 class Web3AuthMFAService {
 
     // if the device share is missing and also no password has been provided
@@ -23,60 +19,92 @@ class Web3AuthMFAService {
     private var isAlreadyMFA = true
     private let postBoxKey: String
     private let publicAddress: String
-    private var thresholdKey: ThresholdKey?
+    private var thresholdKey: ThresholdKey
     private var threshold = 0
+    let question = "PASSWORD"
 
     static func mfaEnabled(userInfo: [String: Any]) -> Bool {
-
-        // in future version od the SDK there will be an entry called "nonce". If 0, then mfsa is enabled
+        // TODO: in future version od the SDK there will be an entry called "nonce". If 0, then mfa is enabled
 
         // For now we whitelist adresses
-
         let email = userInfo["email"] as? String
 
-        return email == "bttr.tstr@gmail.com" || email == "ego.other@gmail.com"
+        return email == "bttr.tstr@gmail.com" || email == "ego.other@gmail.com" || email == "dirk@safe.global"
     }
 
-    // this activates MFA if not already activated
-    func setupPassword(password: String) {
-
-        // check if there is already a password set?
-
+    func setupPassword(password: String) async throws {
+        let _ = try await SecurityQuestionModule.generate_new_share(threshold_key: thresholdKey, questions: question, answer: password)
+        LogService.shared.debug("---> password set for the first time")
     }
 
-    // this activates MFA if not already activated
-    func setupTrustedDevice() {
-
-
-    }
-
-    // if there is no device share, we need a password to restore the final key
-    func inputPassword(password: String) {
+    /// this is used to unlock the key. Can also be done in init()
+    func inputPassword(password: String) async throws {
         self.password = password
+
+        // password given
+        guard let result = try? await SecurityQuestionModule.input_share(threshold_key: thresholdKey, answer: password) else {
+            throw("input share failed. Make sure threshold key is initialized")
+        }
+
+        LogService.shared.debug("---> password correct")
+        if !result {
+            throw("password incorrect")
+        }
+    }
+
+    func changePassword(oldPassword: String, newPassword: String) async throws {
+        // password given
+        guard let result = try? await SecurityQuestionModule.input_share(threshold_key: thresholdKey, answer: oldPassword) else {
+            throw("input share failed. Make sure threshold key is initialized")
+        }
+        if !result {
+            throw("old password incorrect")
+        } else {
+            LogService.shared.debug("---> old password correct")
+        }
+
+        guard let reconstructionDetails = try? await thresholdKey.reconstruct() else {
+            throw("Failed to reconstruct key. More share(s) required.")
+        }
+        LogService.shared.debug("---> key reconstructed: \(reconstructionDetails)")
+
+        let changeResult = try await SecurityQuestionModule.change_question_and_answer(threshold_key: thresholdKey, questions: question, answer: newPassword)
+
+        if changeResult {
+            LogService.shared.debug("---> password changed")
+        } else {
+            LogService.shared.debug("---> password not changed")
+        }
     }
 
 
-
-    init(postBoxKey: String, publicAddress: String, password: String? = nil) async throws {
+    /// Instantiate a Web3AuthMFAService object.
+    /// If the postboxkey was not yet migrated it will be migrated to MFA.
+    /// Even if the flag `initialize` is not given. Unfortunately we cannot detect reliably if a key is SFA or MFA.
+    /// And calling ThresholdKey.initialize() will convert it.
+    ///
+    /// - Parameters:
+    ///   - postBoxKey: Key returned by CustomAuth
+    ///   - publicAddress: Used as a key to store the shares in the KeyChain
+    ///   - password: If given used as one share (optional)
+    ///   - initialize: If given, init() assumes it is called on a postBoxKey that is still on SFA.
+    ///
+    /// - Returns: `Web3AuthMFAService`
+    ///
+    /// - Throws: `RuntimeError`, indicates the setup failed. Password wrong, not enough shares to recover key, etc.
+    init(postBoxKey: String,
+         publicAddress: String,
+         password: String? = nil, // if not nil, use to create a password share on initialize otherwise used to unlock the key
+         initialize: Bool = false
+    ) async throws {
 
         self.postBoxKey = postBoxKey
-        print("---> postboxkey: \(postBoxKey)")
+        LogService.shared.debug("---> postboxkey: \(postBoxKey)")
         self.publicAddress = publicAddress
-        print("---> publicAddress: \(publicAddress)")
+        LogService.shared.debug("---> publicAddress: \(publicAddress)")
         self.shareMissing = false
         self.password = password
 
-        // TODO look for device share
-
-        if isAlreadyMFA {
-            // prepare
-            try await setup()
-        }
-
-    }
-
-    private func setup() async throws {
-        print("---> setup()")
         guard let storage_layer = try? StorageLayer(enable_logging: true, host_url: "https://metadata.tor.us", server_time_offset: 2) else {
             throw("Failed to create storage layer")
         }
@@ -95,80 +123,84 @@ class Web3AuthMFAService {
 
         self.thresholdKey = thresholdKey
 
-        //        Task {
+        if initialize {
+            try await initialSetup()
+        } else {
+            try await setup()
+        }
+    }
+
+    private func initialSetup() async throws {
+
+        // This is the first time initialize is called on a threshold key derived by this postbox key. That means a device share is created automatically.
+        // We must save this device share to the keychain if this is a trudted device. Otherwise we do not save it.
+        // we must add the password share if we're given a password
+
+        LogService.shared.debug("---> initialSetup()")
+
         guard let key_details = try? await thresholdKey.initialize(never_initialize_new_key: false, include_local_metadata_transitions: false) else {
             throw("Failed to get key details")
         }
-        //        totalShares = Int(key_details.total_shares)
-        threshold = Int(key_details.threshold)
-        //        tkeyInitalized = true
 
-        print("---> threshold: \(threshold)")
-
-        if let deviceShare = try? KeychainInterface.fetch(key: "\(publicAddress):device-key") {
-            // device share found
-
-
-            try await thresholdKey.input_share(share: deviceShare, shareType: nil)
-
-        } else {
-            // no device share found
-            print("---> no device share found")
-            //                guard let reconstructionDetails = try? await thresholdKey.reconstruct() else {
-            //                     throw("Failed to reconstruct key. \(key_details.required_shares) more share(s) required. If you have security question share, we suggest you to enter security question PW to recover your account")
-            //                }
-            //                var shareIndexes = try thresholdKey.get_shares_indexes()
-            //                shareIndexes.removeAll(where: {$0 == "1"}) // apparently 1 is the postbox share
-            //
-            //                guard let share = try? thresholdKey.output_share(shareIndex: shareIndexes[0], shareType: nil) else {
-            //                    throw("Failed to output share")
-            //                }
-            //
-            //                guard let _ = try? KeychainInterface.save(item: share, key: "\(publicAddress):device-key") else {
-            //                    throw("Failed to save share")
-            //                }
-            //
-            //                guard let reconstructionDetails = try? await thresholdKey.reconstruct() else {
-            //                    throw("Failed to reconstruct key. \(key_details.required_shares) more share(s) required.")
-            //                }
-
-
-            // try await thresholdKey.input_share(share: passwordShare, shareType: nil)
-
-
-
-
+        guard let reconstructionDetails = try? await thresholdKey.reconstruct() else {
+            throw("Failed to reconstruct key. \(key_details.required_shares) more share(s) required.") // this should not happen when using initialsetup on an SFA-Key
         }
+
+        var shareIndexes = try thresholdKey.get_shares_indexes()
+        shareIndexes.removeAll(where: {$0 == "1"}) // apparently 1 is the postboxkey share
+
+        guard let share = try? thresholdKey.output_share(shareIndex: shareIndexes[0], shareType: nil) else {
+            throw("Failed to output share")
+        }
+        guard let _ = try? KeychainInterface.save(item: share, key: "\(publicAddress):device-key") else {
+            throw("Failed to save share")
+        }
+
         if let password = password {
+            let question = "what's your password?"
+            let _ = try await SecurityQuestionModule.generate_new_share(threshold_key: thresholdKey, questions: question, answer: password)
+            LogService.shared.debug("---> password correct or set for the first time")
+        }
+    }
+
+    /// set up a key to be used as an MFA key
+    private func setup() async throws {
+        LogService.shared.debug("---> setup()")
+
+        // Find device share in Keychain
+        let deviceShare = try? KeychainInterface.fetch(key: "\(publicAddress):device-key")
+
+        guard let key_details = try? await thresholdKey.initialize(never_initialize_new_key: false, include_local_metadata_transitions: false) else {
+            throw("Failed to get key details")
+        }
+        threshold = Int(key_details.threshold)
+        LogService.shared.debug("---> threshold: \(threshold)")
+
+        if let deviceShare = deviceShare {
+            // device share found
+            LogService.shared.debug("---> device share found")
+            try await thresholdKey.input_share(share: deviceShare, shareType: nil)
+        }
+
+        if let password = password {
+            // password given
             guard let result = try? await SecurityQuestionModule.input_share(threshold_key: thresholdKey, answer: password) else {
                 throw("input share failed. Make sure threshold key is initialized")
             }
-            print("---> password correct")
+
+            LogService.shared.debug("---> password correct")
             if !result {
                 throw("password incorrect")
             }
         }
 
-
         guard let reconstructionDetails = try? await thresholdKey.reconstruct() else {
             shareMissing = true
             throw("Failed to reconstruct key with available shares.")
         }
-        print("---> key reconstructed.")
+        LogService.shared.debug("---> key reconstructed.")
         shareMissing = false
 
         finalKey = reconstructionDetails.key
-
-
     }
-
-
-
-
-
-    //    }
-
-
-
-    
 }
