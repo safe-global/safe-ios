07986148fb14b203ae1d04c9d73fc2d5f03aade8
Dirk Jäckel <dirk@safe.global>
2023-08-09 20:41:39 +0200

    tests ready to run

--- MultisigTests/UI/Safe Management/Create Safe/Web3AuthMFAServiceTest.swift
+++ MultisigTests/UI/Safe Management/Create Safe/Web3AuthMFAServiceTest.swift
@@ -20,12 +20,12 @@ class Web3AuthMFAServiceTests: XCTestCase {
 
     func testInitWithDeviceShare() async throws {
         // init with device share
-        keychain.map["0x875b5EAAC06a857d1046cdA1b2a6683deeFbA5B4:device-key"] = "share"
+        keychain.map["0x875b5EAAC06a857d1046cdA1b2a6683deeFbA5B4:device-key"] = "66665182a2b416cfe448413513dfc6ee8d87538b8d678bde30177c1f463aa822"
         try await web2authMFAService = Web3AuthMFAService(postBoxKey: "c57c57f1a3463f14fb6ce79835f5df8437a8d449b5e2219aa2bb3876554f99cb",
                                                 publicAddress: "0x875b5EAAC06a857d1046cdA1b2a6683deeFbA5B4",
                                                 keychainInterface: keychain)
         try await web2authMFAService.reconstruct()
-        XCTAssert(web2authMFAService.finalKey == "")
+        XCTAssert(web2authMFAService.finalKey == "75907aad04675827696b92ca481c6b00a24514d8b8819c97840c1960a50f7126")
 
     }
 
@@ -36,9 +36,8 @@ class Web3AuthMFAServiceTests: XCTestCase {
                                                 password: "foobar23",
                                                 keychainInterface: keychain)
         try await web2authMFAService.reconstruct()
-        XCTAssert(web2authMFAService.finalKey == "")
+        XCTAssert(web2authMFAService.finalKey == "75907aad04675827696b92ca481c6b00a24514d8b8819c97840c1960a50f7126")
     }
-
 }
 
 class TestKeychainInterface: KeychainInterface {
