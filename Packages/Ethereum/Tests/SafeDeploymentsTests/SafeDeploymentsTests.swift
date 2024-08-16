bb1554a8d63755982a2c379e52d7b6744f365e2e
Dmitry Bespalov <dima@safe.global>
2024-08-16 15:36:33 +0200

    Revert "GH-3441 Updated the assets"

--- Packages/Ethereum/Tests/SafeDeploymentsTests/SafeDeploymentsTests.swift
+++ Packages/Ethereum/Tests/SafeDeploymentsTests/SafeDeploymentsTests.swift
@@ -16,11 +16,5 @@ class SafeDeploymentsTests: XCTestCase {
             return
         }
         XCTAssertEqual(gs.contractName, "GnosisSafeL2")
-        // "canonical"
-        XCTAssertEqual(gs.address(for: "1"), "0x3E5c63644E683549055b9Be8653de26E0B4CD36E")
-        // "eip155"
-        XCTAssertEqual(gs.address(for: "18"), "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA")
-        // "zksync"
-        XCTAssertEqual(gs.address(for: "280"), "0x1727c2c531cf966f902E5927b98490fDFb3b2b70")
     }
 }
